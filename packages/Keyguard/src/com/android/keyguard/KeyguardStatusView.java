c205165bee8f11d5c9533d091f4bbbaada2edd2d
Lars Greiss <kufikugel@googlemail.com>
2014-12-08 20:17:49 +0100

    Don't refresh ui when screen off
    KeyguardStatusView is doing refresh all the time,
    which cause high power when screen off
    
    PatchSet:
    - We turn of the refresh when screen is off which is a good
      idea. But we should force latest next refresh when screen turns
      on. Du that a timechanged refresh maybe comes a bit later.
    
    Change-Id: Icbb9a92bae3cf099417ab820617dd3f17e49f75a

--- packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
@@ -75,11 +75,18 @@ public class KeyguardStatusView extends GridLayout implements
 
     private WeatherController mWeatherController;
 
+    //On the first boot, keygard will start to receiver TIME_TICK intent.
+    //And onScreenTurnedOff will not get called if power off when keyguard is not started.
+    //Set initial value to false to skip the above case.
+    private boolean mEnableRefresh = false;
+
     private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
 
         @Override
         public void onTimeChanged() {
-            refresh();
+            if (mEnableRefresh) {
+                refresh();
+            }
         }
 
         @Override
@@ -94,11 +101,14 @@ public class KeyguardStatusView extends GridLayout implements
         @Override
         public void onStartedWakingUp() {
             setEnableMarquee(true);
+            mEnableRefresh = true;
+            refresh();
         }
 
         @Override
         public void onFinishedGoingToSleep(int why) {
             setEnableMarquee(false);
+            mEnableRefresh = false;
         }
 
         @Override
