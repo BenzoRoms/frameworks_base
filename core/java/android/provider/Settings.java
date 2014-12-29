e1dc9f299856a2ca8ca1b43d03fe57e38f94a46c
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    AOKP custom system animations (1/2)
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    
    Change-Id: Ibeaa0f369279bfa94f2547c423483f7eff80bcb8
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3699,6 +3699,24 @@ public final class Settings {
         */
         public static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
 
+        /**  
+         * AOKP Custom System Animations
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
+                "activity_open",
+                "activity_close",
+                "task_open",  
+                "task_close",  
+                "task_to_front",
+                "task_to_back",  
+                "wallpaper_open",  
+                "wallpaper_close",  
+                "wallpaper_intra_open",  
+                "wallpaper_intra_close",  
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
