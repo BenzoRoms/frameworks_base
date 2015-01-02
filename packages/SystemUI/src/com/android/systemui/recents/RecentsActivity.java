98227fe3f815284178fb2b85107b86a321d0157c
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-02 00:01:05 +0000

    SystemUI: replace recents clear all button with a fab button
    * this allows us to have it any corner
    * added bottom right/left options
    
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    Change-Id: I1016cae52d22d815e0a8fe4d83dda60e05a9cf51

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -259,15 +259,15 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
                 }
             });
             mRecentsView.setSearchBarVisibility(View.GONE);
-            findViewById(R.id.clear_recents).setVisibility(View.GONE);
+            findViewById(R.id.floating_action_button).setVisibility(View.GONE);
         } else {
             if (mEmptyView != null) {
                 mEmptyView.setVisibility(View.GONE);
                 mEmptyView.setOnClickListener(null);
             }
-            findViewById(R.id.clear_recents).setVisibility(View.VISIBLE);
+            findViewById(R.id.floating_action_button).setVisibility(View.VISIBLE);
             if (mRecentsView.hasValidSearchBar()) {
-                mRecentsView.setSearchBarVisibility(mRecentsSearchbar ? View.VISIBLE);
+                mRecentsView.setSearchBarVisibility(mRecentsSearchbar ? View.VISIBLE : View.GONE);
             } else {
                 refreshSearchWidgetView();
             }
