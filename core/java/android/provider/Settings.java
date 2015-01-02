d8aed7c20d3bc959e910a1f5290b065c111520f3
Jorge Martín <angel.arasthel@gmail.com>
2015-01-02 12:10:42 +0100

    Quick Settings: add option to turn off main tiles [1/2]
    I removed the "dual" status that was hardcoded into Wifi and BT tiles and added it
    as a variable which is true when the tile is on the first row.
    
    Also, the first row is now hardcoded to have only 2 items to follow the same design as before.
    
    Inversion and Hotspot tile are no longer contextual and can be added, removed or rearranged at will.
    
    Settings: http://review.cyanogenmod.org/83418
    
    Change-Id: Ie596a54a5c4eb777782fb047496cfcd0a6cd2e89

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5979,6 +5979,13 @@ public final class Settings {
                 "camera_double_tap_power_gesture_disabled";
 
         /**
+         * Use "main" tiles on the first row of the quick settings panel
+         * 0 = no, 1 = yes
+         * @hide
+         */
+        public static final String QS_USE_MAIN_TILES = "sysui_qs_main_tiles";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
