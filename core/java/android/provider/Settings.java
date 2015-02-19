4ad0cd769c811440f6e8b8040475c9e74630063f
maxwen <max.weninger@gmail.com>
2015-02-19 15:15:49 -0800

    Add OmniSwitch as option for default recents [1/2]
    I don't know who the original author of this commit is, but they deserve the credit
    
    Change-Id: I6e7e1a493ab4f96abc13439466b63ee9ad1b8e5f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4073,6 +4073,12 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_BAR_BATTERY_LOW_COLOR = "statusbar_battery_bar_battery_low_color";
 
         /**
+         * Alternative recent apps integration using OmniSwitch
+         * @hide
+         */
+        public static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
