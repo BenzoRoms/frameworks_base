8a08b4da551de5edac9242c958751e4420bc4851
maxwen <max.weninger@gmail.com>
2015-03-08 21:26:14 +0100

    Wakelock Blocker [1/2]
    Change-Id: Iafc48d380d8789f48c272fa052b9a39421617086

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3617,6 +3617,18 @@ public final class Settings {
          */
         public static final String MENU_VISIBILITY = "menu_visibility";
 
+	/**
+	 * Enable blocking wakelock
+	 * @hide
+	 */
+	public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+	/**
+	 * List of wakelock blocks selected
+	 * @hide
+	 */
+	public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
