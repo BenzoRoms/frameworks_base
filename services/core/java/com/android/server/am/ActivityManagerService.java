de8d4be98b8721052b96e71297919bcfb1906ce0
daegeun.song <daegeun.song@lge.com>
2015-05-07 15:32:27 +0900

    Set swappiness only BG_NONINTERACTIVE group.
    Set swappiness only BG_NONINTERACTIVE group.
    
    Change-Id: I10ecd8a6fadd4aa6661f1f9e1f1080ee30dec4e3
    Signed-off-by: Daegeun Song <daegeun.song@lge.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -18850,7 +18850,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                     }
                 }
                 Process.setSwappiness(app.pid,
-                        app.curSchedGroup <= Process.THREAD_GROUP_BG_NONINTERACTIVE);
+                        app.curSchedGroup == Process.THREAD_GROUP_BG_NONINTERACTIVE);
             }
         }
         if (app.repForegroundActivities != app.foregroundActivities) {
