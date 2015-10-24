6727b996d2a3f333cfd89ceba68919825a1bbec6
mike <mikew8978@gmail.com>
2015-10-24 03:43:32 -0400

    FWB: Navigation bar button color (1/2)
    original commit https://github.com/AOSIP/platform_frameworks_base/commit/14ddb7d2e9f503b965446d2c561102f0410f7bc4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4251,6 +4251,12 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
 
         /**
+         * Navigation bar button color
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_TINT = "navigation_bar_tint";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
