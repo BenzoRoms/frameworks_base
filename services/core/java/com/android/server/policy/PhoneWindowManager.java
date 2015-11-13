4a8bb6a74831e7d2060940c9f188928bcb0ec56c
d34d <clark@cyngn.com>
2015-11-13 09:08:13 -0800

    ExpandedDesktop: Use WindowManagerPolicyControl
    A PolicyControl static methods were still being used, this patch
    makes sure we are using WindowManagerPolicyControl instead.
    
    Change-Id: Iac9dac8e356f2948506ded9bf959e86608083744

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -120,6 +120,7 @@ import android.view.accessibility.AccessibilityManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.WindowManagerPolicyControl;
 import android.widget.Toast;
 
 import com.android.internal.R;
