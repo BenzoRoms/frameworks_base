111cd60616572dfa8163deeba70f73f0eb546546
lichti1901 <lichti190185@gmail.com>
2015-11-25 19:43:41 +0100

    National Data Roaming [3/3]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5011,6 +5011,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
         /**
+         * Whether national data roaming should be used.
+         * @hide
+         */
+        public static final String MVNO_ROAMING = "mvno_roaming";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
