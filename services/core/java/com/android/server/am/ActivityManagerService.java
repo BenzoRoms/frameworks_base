4def3c780bce5a2e87aa86c8bbf8ee2ef36f2522
riddle_hsu <riddle_hsu@htc.com>
2015-12-03 21:13:25 +0800

    Fix NPE by not restore task record correctly.
    This also fixes task data does not read fully from xml.
    The original checking "in.getDepth() < outerDepth" is
    inversed that can only read one tag if there is more depth.
    
    e.g. There is affinityIntent with category, then only it
    will be read. The task record will have no intent, and when
    there is some packages changed, find match package in recent
    tasks will meet NPE.
    
    https://code.google.com/p/android/issues/detail?id=196142
    
    Related commit: 21d24a21ea4aaadd78e73de54168e8a8a8973e4d
    
    Change-Id: I6c4fb798ed92ed49f284547e1581ff749db256ea

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -8767,7 +8767,7 @@ public final class ActivityManagerService extends ActivityManagerNative
             TaskRecord tr = mRecentTasks.get(i);
             if (tr.userId != userId) continue;
 
-            ComponentName cn = tr.intent.getComponent();
+            ComponentName cn = tr.getBaseIntent().getComponent();
             if (cn != null && cn.getPackageName().equals(packageName)) {
                 // If the package name matches, remove the task.
                 removeTaskByIdLocked(tr.taskId, true);
@@ -8784,7 +8784,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                 continue;
             }
 
-            ComponentName cn = tr.intent.getComponent();
+            ComponentName cn = tr.getBaseIntent().getComponent();
             final boolean sameComponent = cn != null && cn.getPackageName().equals(packageName)
                     && (filterByClasses == null || filterByClasses.contains(cn.getClassName()));
             if (sameComponent) {
