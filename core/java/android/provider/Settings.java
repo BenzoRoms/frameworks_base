8dea68b1e81748dfe24bdf62bf238280eb30d27f
mydongistiny <jaysonedson@gmail.com>
2015-12-17 14:55:22 -0800

    base: Unhide Breathing SMS

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3298,7 +3298,6 @@ public final class Settings {
 
         /**
          * Give SMS notifications a breathing effect
-         * @hide
          */
         public static final String KEY_SMS_BREATH = "sms_breath";
 
