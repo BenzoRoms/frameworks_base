8013dc6ae5e0a733baed0ac2d07a4e0932a6c5e5
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-12-25 23:39:19 +0100

    [2/2] frameworks_base: Code: Add options for battery icon and text in status bar
    Ported to M by Yank555.lu
    
    Available options :
    
    - icon : portrait, landscape battery / normal, dotted circle / text only / none
    - battery percent : none / inside icon / next to icon
    
    Charging with portrait and landscape icons :
    
    - without battery percent inside --> show a bolt inside of icon blinking
    - with battery percent inside --> alternate every second between battery percent and bolt inside of icon
    
    Charging with circle icon :
    
    - without battery percent inside --> charged subset of circle will rotate with bolt inside blinking
    - with battery percent inside --> charged subset of circle will rotate with bolt and battery percent alternating inside
    
    Charging text only :
    
    - will show 85~ instead of 85% when charging
    
    To do for v1 :
    
    - nothing, can be merged from my point of view
    
    To do for v2 :
    
    - animate battery percent for text only style
    - rewrite battery drawing code to properly partially invert text/bolt
    
    Based on Zavon Joseph patchset 5 (SlimLP)
    
    Extended with text option by Jean-Pierre Rasquin <yank555.lu@gmail.com> :
    
    - fixed text not showing on lockscreen at all
    
    - fixed always displaying text when charging on lockscreen bug, now respects user setting on lockscreen as well
    
    - fixed text not showing on expanded notifications
    
    - fixed text only won't stick, will show portrait battery plus text on reboot, settings will reflect last user setting, and be different to what is actually applied
    
    - fixed text will always show on expanded notifications, even when quick tiles are not showing
    
    - fixed white on white landscape in icon battery text (text will switch color as soon as 25% is reached)
    
    - added only show text next to icon in expanded notifications when percent is not shown in the icon, else it's redundant
    
    - fixed code layout
    
    - added charging animation for portrait / landscape icon when battery percent is set to 'inside icon'
    
    - fixed charging bolt not correctly showing on landscape icon
    
    - added charging animation (battery percent - bolt) to circle battery icon
    
    - fixed my derp that never showed charge bolt when battery percent was set to hidden or next to icon
    
    - added animate bolt even when battery percent set to hidden
    
    - fixed charged part of battery icon not drawn between hardcoded critical level (15) and battery safe mode level
    
    - fixed battery percent not shown when not opaque (low battery, white color)
    
    - maxed out circle battery icon size (thx to Griffin Millender)
    
    - added dotted battery icon style (thx to Griffin Millender)
    
    - removed two (make that three) unnecessary observers (thx to Griffin Millender)
    
    - fixed the missing update to notification battery icon (thx to Griffin Millender)
    
    - tiny code cleanup in BatteryMeterView.java
    
    - removed BatteryLevelTextView class, this is not needed
    
    - removed one more unnecessary observer (in BatteryLevelTextView)
    
    - added updating battery level text from PhoneStatusBar to StatusBarHeaderView and KeyguardStatusBarView  to avoid the user actually seeing the update when pulling down notifications and battery has dropped since last time notifications were pulled and getting to lockscreen (unlikely, but this way it's settled for good)
    
    - added battery style text only charging feedback (preliminary, will show 85~ instead of 85%)
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -78,6 +78,7 @@ public class StatusBarIconController implements Tunable {
     private View mNotificationIconArea;
     private ImageView mMoreIcon;
     private BatteryMeterView mBatteryMeterView;
+    private TextView mBatteryLevelText;
     private Clock mClock;
     private Clock mCenterClock;
     private Clock mLeftClock;
@@ -127,6 +128,7 @@ public class StatusBarIconController implements Tunable {
         mNotificationIcons.setOverflowIndicator(mMoreIcon);
         mStatusIconsKeyguard = (LinearLayout) keyguardStatusBar.findViewById(R.id.statusIcons);
         mBatteryMeterView = (BatteryMeterView) statusBar.findViewById(R.id.battery);
+        mBatteryLevelText = (TextView) statusBar.findViewById(R.id.battery_level_text);
         mClock = (Clock) statusBar.findViewById(R.id.clock);
         mCenterClockLayout = (LinearLayout)statusBar.findViewById(R.id.center_clock_layout);
         mCenterClock = (Clock) statusBar.findViewById(R.id.center_clock);
@@ -438,6 +440,7 @@ public class StatusBarIconController implements Tunable {
         mSignalCluster.setIconTint(mIconTint, mDarkIntensity);
         mMoreIcon.setImageTintList(ColorStateList.valueOf(mIconTint));
         mBatteryMeterView.setDarkIntensity(mDarkIntensity);
+        mBatteryLevelText.setTextColor(mIconTint);
 
         applyClockColorTint();
         applyNotificationIconsTint();
