3136db631131b8a2772f5e462a217de891aa61a5
mydongistiny <jaysonedson@gmail.com>
2016-01-10 18:55:23 -0800

    am: Fix context derp in ActivityManagerService

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1445,7 +1445,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                     if (mShowDialogs && !mSleeping && !mShuttingDown) {
                         if (Settings.System.getInt(mContext.getContentResolver(),
                                 Settings.System.DISABLE_FC_NOTIFICATIONS, 0) != 1) {
-                            Dialog d = new AppErrorDialog(getUiContext(),
+                            Dialog d = new AppErrorDialog(mContext,
                                     ActivityManagerService.this, res, proc);
                             d.show();
                             proc.crashDialog = d;
