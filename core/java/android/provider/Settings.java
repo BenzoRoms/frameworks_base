47013daa81081a2da3858cd25065af1198bd010a
Alex Cruz <mazdarider23@gmail.com>
2016-01-21 11:14:04 -0500

    Configurable dashboard columns [2/2]
    Change-Id: Icb79592201ed3f8fa097ab3fb9e919dd98b556d1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4498,6 +4498,12 @@ public final class Settings {
         public static final String BLOCKED_BY_RECENTS = "blocked_by_recents";
 
         /**
+         * Change the number of columns displayed in the Settings dashboard
+         * @hide
+         */
+        public static final String DASHBOARD_COLUMNS = "dashboard_columns";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
