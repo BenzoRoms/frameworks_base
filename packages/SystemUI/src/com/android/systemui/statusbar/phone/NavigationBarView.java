0619f1cbbc9e59f91d6f1fe46f5eae79c6263d0d
LorDClockaN <davor@losinj.com>
2016-02-08 19:51:58 +0100

    Long click navbar camera action to open default gallery app
    Change-Id: Ie2296db59512534f038a665bce9267d10a8e900f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -153,6 +153,7 @@ public class NavigationBarView extends LinearLayout {
     private OnLongClickListener mNotificationsLongListener;
     private OnClickListener mTorchClickListener;
     private OnClickListener mCameraClickListener;
+    private OnLongClickListener mCameraLongClickListener;
     private OnClickListener mScreenShotClickListener;
     private OnClickListener mImmersiveClickListener;
     private OnClickListener mAppPickerClickListener;
@@ -857,8 +858,9 @@ public class NavigationBarView extends LinearLayout {
                       OnLongClickListener recentsBackListener, OnTouchListener homeSearchActionListener,
                       OnLongClickListener longPressHomeListener, OnClickListener notificationsClickListener,
                       OnLongClickListener notificationsLongListener, OnClickListener torchClickListener,
-                      OnClickListener cameraClickListener, OnClickListener screenshotClickListener,
-                      OnClickListener immersiveClickListener, OnClickListener appPickerClickListener) {
+                      OnClickListener cameraClickListener, OnLongClickListener cameraLongClickListener,
+                      OnClickListener screenshotClickListener, OnClickListener immersiveClickListener,
+                      OnClickListener appPickerClickListener) {
         mRecentsClickListener = recentsClickListener;
         mRecentsPreloadListener = recentsPreloadListener;
         mHomeSearchActionListener = homeSearchActionListener;
@@ -868,6 +870,7 @@ public class NavigationBarView extends LinearLayout {
         mNotificationsLongListener = notificationsLongListener;
         mTorchClickListener = torchClickListener;
         mCameraClickListener = cameraClickListener;
+        mCameraLongClickListener = cameraLongClickListener;
         mScreenShotClickListener = screenshotClickListener;
         mImmersiveClickListener = immersiveClickListener;
         mAppPickerClickListener = appPickerClickListener;
@@ -925,6 +928,8 @@ public class NavigationBarView extends LinearLayout {
         View cameraView = mCurrentView.findViewWithTag(NavbarEditor.NAVBAR_CAMERA);
         if (cameraView != null) {
             cameraView.setOnClickListener(mCameraClickListener);
+            cameraView.setLongClickable(true);
+            cameraView.setOnLongClickListener(mCameraLongClickListener);
         }
         View screenshotView = mCurrentView.findViewWithTag(NavbarEditor.NAVBAR_SCREENSHOT);
         if (screenshotView != null) {
