28477da79675cfabc47665cb911b813afedce672
LorDClockaN <davor@losinj.com>
2016-02-08 22:41:01 +0100

    FWB: Add rotation animation to navbar icons (1/2)
    Change-Id: I0fe3eb0b8cfb891fa1b940c37710116544b447a5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4995,6 +4995,12 @@ public final class Settings {
         public static final String FORMAL_TEXT_INPUT = "formal_text_input";
 
         /**
+         * Add rotation to navbar buttons on navbar recreate (rotate screen, etc)
+         * @hide
+         */
+        public static final String NAV_BUTTONS_ROTATION = "nav_buttons_rotation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
