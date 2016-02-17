61c2d9e727cc4c3d1d095301fce1ad4b8e17337c
Ankit Gupta <agupta@cyngn.com>
2016-02-17 15:48:17 -0800

    SettingsProvider: Allow default volume adjust sound to be overlayed.
    Issue-id: YAM-145
    Change-Id: I06394c4fcfca5978b50a4186935ac6bda7b94b16

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5269,6 +5269,12 @@ public final class Settings {
          */
         public static final String LONG_PRESS_KILL_DELAY = "long_press_kill_delay";
 
+	/**
+         * Whether the phone volume up / down effect should be played
+         * @hide
+         */
+        public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
