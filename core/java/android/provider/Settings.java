7c7972e59fca21c57bf9583093a126c1320dd83b
Alex Cruz <mazdarider23@gmail.com>
2016-02-17 10:39:13 -0500

    Dashoard tileview dividers toggle [1/2]
    Change-Id: I89869162329a87235603e6c71be656464ae6fe66

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5096,6 +5096,12 @@ public final class Settings {
         public static final String TRANSPARENT_POWER_DIALOG_DIM = "transparent_power_dialog_dim";
 
         /**
+         * Whether to display dashboard dividers
+         * @hide
+         */
+        public static final String DASHBOARD_TILEVIEW_DIVIDERS = "dashboard_tileview_dividers";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
