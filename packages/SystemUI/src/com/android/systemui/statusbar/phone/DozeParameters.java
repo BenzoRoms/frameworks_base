e4cfe8f031012c3a0e9714fb43a3a1f2755d0942
beanstown106 <nbenis106@gmail.com>
2016-03-05 00:08:49 -0500

    Custom ambient display settings (1/2)
    *This is a rehash of parts of slims and parts of omni's ambient display mods
    *Added my own little twist on some of the things
    *Moved all options into a seperate ambient display fragment
    
    Features:
    *default/user config switch
    *Fade in duration
    *Fade out duration
    *Visibility duration
    *Option to disable pulse on notifications
    *Option to disable pulse with pickup sensor
    
    Credits:
    Maxwen(omni)
    Squadzone(omni)
    Dankoman(slim)
    
    Change-Id: I9e578b5a70fb23f65afdcb92aff081ec56509e0b
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeParameters.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeParameters.java
@@ -18,6 +18,8 @@ package com.android.systemui.statusbar.phone;
 
 import android.content.Context;
 import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.MathUtils;
@@ -80,6 +82,13 @@ public class DozeParameters {
         }
     }
 
+    public boolean getOverwriteValue() {
+        final int values = Settings.System.getIntForUser(mContext.getContentResolver(),
+               Settings.System.DOZE_OVERWRITE_VALUE, 0,
+                    UserHandle.USER_CURRENT);
+        return values != 0;
+    }
+
     public boolean getDisplayStateSupported() {
         return getBoolean("doze.display.supported", R.bool.doze_display_state_supported);
     }
@@ -89,16 +98,31 @@ public class DozeParameters {
     }
 
     public int getPulseInDuration(boolean pickup) {
+        if (getOverwriteValue()) {
+            return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.DOZE_PULSE_DURATION_IN, 500,
+                    UserHandle.USER_CURRENT);
+        }
         return pickup
                 ? getInt("doze.pulse.duration.in.pickup", R.integer.doze_pulse_duration_in_pickup)
                 : getInt("doze.pulse.duration.in", R.integer.doze_pulse_duration_in);
     }
 
     public int getPulseVisibleDuration() {
+        if (getOverwriteValue()) {
+            return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.DOZE_PULSE_DURATION_VISIBLE, 3000,
+                    UserHandle.USER_CURRENT);
+        }
         return getInt("doze.pulse.duration.visible", R.integer.doze_pulse_duration_visible);
     }
 
     public int getPulseOutDuration() {
+        if (getOverwriteValue()) {
+            return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.DOZE_PULSE_DURATION_OUT, 500,
+                    UserHandle.USER_CURRENT);
+        }
         return getInt("doze.pulse.duration.out", R.integer.doze_pulse_duration_out);
     }
 
@@ -111,6 +135,12 @@ public class DozeParameters {
     }
 
     public boolean getPulseOnPickup() {
+        if (getOverwriteValue()) {
+            final int values = Settings.System.getIntForUser(mContext.getContentResolver(),
+                   Settings.System.DOZE_PULSE_ON_PICKUP, 1,
+                    UserHandle.USER_CURRENT);
+            return values != 0;
+        }
         return getBoolean("doze.pulse.pickup", R.bool.doze_pulse_on_pick_up);
     }
 
@@ -123,6 +153,12 @@ public class DozeParameters {
     }
 
     public boolean getPulseOnNotifications() {
+        if (getOverwriteValue()) {
+            final int values = Settings.System.getIntForUser(mContext.getContentResolver(),
+                   Settings.System.DOZE_PULSE_ON_NOTIFICATIONS, 1,
+                    UserHandle.USER_CURRENT);
+            return values != 0;
+        }
         return getBoolean("doze.pulse.notifications", R.bool.doze_pulse_on_notifications);
     }
 
@@ -248,6 +284,17 @@ public class DozeParameters {
         public boolean isIn(int value) {
             return (mIsIn.get(value, mDefaultIsIn));
         }
+    }
+
+    public int getDozeBrightness() {
+	final int dozeBrightnessDefault = mContext.getResources().getInteger(
+                    com.android.internal.R.integer.config_screenBrightnessDoze);
+        if (getOverwriteValue()) {
+            return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.DOZE_SCREEN_BRIGHTNESS, dozeBrightnessDefault,
+                    UserHandle.USER_CURRENT);
+        }
+        return dozeBrightnessDefault;
     }
 
     public static class PulseSchedule {
