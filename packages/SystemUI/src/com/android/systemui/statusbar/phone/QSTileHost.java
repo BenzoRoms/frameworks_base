eb49bed0038b607fbe51129d3c8e37b5ba4291db
mydongistiny <jaysonedson@gmail.com>
2016-05-08 14:36:51 -0700

    QS: Add ViPER4Android tile

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.ScreenOffTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
+import com.android.systemui.qs.tiles.Viper4AndroidTile;
 import com.android.systemui.qs.tiles.VolumeTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -300,6 +301,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("layers")) return new LayersManagerTile(this);
 	else if (tileSpec.equals("screenrecord")) return new ScreenrecordTile(this);
 	else if (tileSpec.equals("kill_app")) return new KillAppTile(this);
+        else if (tileSpec.equals("v4a")) return  new Viper4AndroidTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -220,6 +220,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("layers")) return R.string.quick_settings_layers_manager_label;
 	else if (spec.equals("screenrecord")) return R.string.quick_settings_screenrecord_label;
 	else if (spec.equals("kill_app")) return R.string.quick_settings_kill_app_label;
+        else if (spec.equals("v4a")) return R.string.quick_settings_v4a_label;
         return 0;
     }
 
@@ -484,6 +485,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("layers")) return R.drawable.ic_qs_layers_manager;
 	    else if (mSpec.equals("screenrecord")) return R.drawable.ic_qs_screenrecord;
  	    else if (mSpec.equals("kill_app")) return R.drawable.ic_qs_app_kill;
+            else if (mSpec.equals("v4a")) return R.drawable.ic_qs_v4a;
             return R.drawable.android;
         }
 
