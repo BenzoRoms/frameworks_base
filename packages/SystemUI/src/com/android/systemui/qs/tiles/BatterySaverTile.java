cf78d4b6982c1d4c8256ee2f201a5358aff35c76
Randall Rushing <randall.rushing@gmail.com>
2016-05-08 00:39:19 -0700

    Tiles: Clean up spaghetti factory in BatterySaver tile
    Change-Id: Id4f5ee71bd58af81b8017a89d4b33e5485d8448f
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BatterySaverTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BatterySaverTile.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2013-2015 The CyanogenMod Project
+ * Copyright (C) 2015 The Euphoria-OS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,29 +20,19 @@ package com.android.systemui.qs.tiles;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.ContentObserver;
-import android.net.Uri;
 import android.os.PowerManager;
-import android.provider.Settings;
 
-import com.android.systemui.qs.QSTile;
 import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
 import com.android.internal.logging.MetricsLogger;
 
 /** Quick settings tile: Battery saver **/
 public class BatterySaverTile extends QSTile<QSTile.BooleanState> {
-
-    private final PowerManager mPm;
-    private boolean mListening;
+    private final PowerManager mPowerMan;
 
     public BatterySaverTile(Host host) {
         super(host);
-        mPm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-    }
-
-    @Override
-    public int getMetricsCategory() {
-        return MetricsLogger.FUELGAUGE_BATTERY_SAVER;
+        mPowerMan = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
     }
 
     @Override
@@ -49,9 +41,26 @@ public class BatterySaverTile extends QSTile<QSTile.BooleanState> {
     }
 
     @Override
+    public void setListening(boolean listening) {
+    }
+
+    @Override
     public void handleClick() {
-        mPm.setPowerSaveMode(!mState.value);
+        mPowerMan.setPowerSaveMode(!mPowerMan.isPowerSaveMode());
         refreshState();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.DISPLAY;
+    }
+
+    @Override
+    protected void handleSecondaryClick() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setClassName("com.android.settings",
+            "com.android.settings.Settings$BatterySaverSettingsActivity");
+        mHost.startActivityDismissingKeyguard(intent);
     }
 
     @Override
@@ -64,49 +73,10 @@ public class BatterySaverTile extends QSTile<QSTile.BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        state.value = mPm.isPowerSaveMode();
         state.visible = true;
         state.label = mContext.getString(R.string.quick_settings_battery_saver);
-        if (state.value) {
-            state.icon = ResourceIcon.get(R.drawable.ic_qs_battery_saver_on);
-            state.contentDescription =  mContext.getString(
-                    R.string.accessibility_quick_settings_battery_saver_on);
-        } else {
-            state.icon = ResourceIcon.get(R.drawable.ic_qs_battery_saver_off);
-            state.contentDescription =  mContext.getString(
-                    R.string.accessibility_quick_settings_battery_saver_off);
-        }
-    }
-
-    @Override
-    protected String composeChangeAnnouncement() {
-        if (mState.value) {
-            return mContext.getString(
-                    R.string.accessibility_quick_settings_battery_saver_changed_on);
-        } else {
-            return mContext.getString(
-                    R.string.accessibility_quick_settings_battery_saver_changed_off);
-        }
-    }
-
-    private ContentObserver mObserver = new ContentObserver(mHandler) {
-        @Override
-        public void onChange(boolean selfChange, Uri uri) {
-            refreshState();
-        }
-    };
-
-    @Override
-    public void setListening(boolean listening) {
-        if (mListening == listening) return;
-        mListening = listening;
-
-        if (listening) {
-            mContext.getContentResolver().registerContentObserver(
-                    Settings.Global.getUriFor(Settings.Global.LOW_POWER_MODE),
-                    false, mObserver);
-        } else {
-            mContext.getContentResolver().unregisterContentObserver(mObserver);
-        }
+        state.icon = ResourceIcon
+                .get(mPowerMan.isPowerSaveMode() ? R.drawable.ic_qs_battery_saver_on
+                        : R.drawable.ic_qs_battery_saver_off);
     }
 }
