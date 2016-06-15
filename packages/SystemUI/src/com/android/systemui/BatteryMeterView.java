e44e22d7b5188c84f99572b3e931d0f0bfbf0e08
Willi Ye <williye97@gmail.com>
2016-06-15 23:56:26 +0200

    BatteryMeterView: Make sure charging animation only gets started on level change
    Change-Id: I6282e77afcf5394c7c2c60ca376706bc1bee8967

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -492,6 +492,7 @@ public class BatteryMeterView extends View implements DemoMode,
 
         private int mLevelAlpha;
         private ValueAnimator mAnimator;
+        private int mLevel;
 
         private boolean mThemeApplied;
 
@@ -561,8 +562,12 @@ public class BatteryMeterView extends View implements DemoMode,
 
         @Override
         public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
-            if (charging && !mThemeApplied && !mChargingAnimationsEnabled) {
+            if (charging && !mThemeApplied && !mChargingAnimationsEnabled
+                    && mLevel != level) {
                 startChargingAnimation(false);
+                mLevel = level;
+            } else {
+                mLevel = 0;
             }
         }
 
