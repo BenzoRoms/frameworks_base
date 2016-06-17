XXMrHyde/android_frameworks_base

Ambient display: Weather, (1/2):
- Option to show/hide the weather
- Option to show/hide the weather location

To do:
Make the weather available on the lock screen,
and remove the weather from status bar when on lock screen

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1366,6 +1366,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         weather.setUp(this, weatherController);
         weatherKeyguard.setUp(this, weatherController);
         mKeyguardStatusBar.setWeatherController(weatherController);
+        mKeyguardStatusView.setWeatherController(weatherController);
 
         ((NetworkTraffic) mStatusBarView.findViewById(R.id.network_traffic_layout))
                 .setNetworkTrafficController(mNetworkTrafficController);

BenzoRoms/frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1356,6 +1356,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mHeader.setNextAlarmController(mNextAlarmController);
         mHeader.setWeatherController(mWeatherController);
 
+        mKeyguardStatusView.setWeatherController(mWeatherController);
+
         PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         mBroadcastReceiver.onReceive(mContext,
                 new Intent(pm.isScreenOn() ? Intent.ACTION_SCREEN_ON : Intent.ACTION_SCREEN_OFF));