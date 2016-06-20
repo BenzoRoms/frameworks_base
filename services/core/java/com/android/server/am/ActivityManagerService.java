276789ffe60dae53e7a31680557ac1f0e4a2b312
Mårten Kongstad <marten.kongstad@sonymobile.com>
2016-06-19 22:33:31 -0400

    Add back assetSeq
    From patchset 3 of:
    OMS: integrate OverlayManagerService into framework
    Author Mårten Kongstad
    https://android-review.googlesource.com/#/c/187459/3

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -17606,6 +17606,10 @@ public final class ActivityManagerService extends ActivityManagerNative
                 }
             } catch (Exception e) {}
         }
+
+        Configuration config = new Configuration(mConfiguration);
+        config.assetSeq++;
+        updateConfiguration(config);
     }
 
     /**
