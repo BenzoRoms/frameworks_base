456eca6ebda8b5c866e6dc734695890de711c66c
Mårten Kongstad <marten.kongstad@sonymobile.com>
2016-07-01 15:06:06 -0400

    OMS: Introduce the OverlayManagerService (Squash) [1/8]
    Squash of the following commits in order.
    
    Make 'idmap --scan' accept more than one input directory
    https://android-review.googlesource.com/#/c/143170/
    
    RRO: idmap: allow flock(2) to block
    https://android-review.googlesource.com/#/c/143142/
    
    Fix lseek argument order.
    Author Elliott Hughes
    https://android-review.googlesource.com/#/c/177730/
    
    Use std::unique_ptr instead of UniquePtr.
    Author Dan Albert
    https://android-review.googlesource.com/#/c/180700/
    
    Runtime resource overlay: support downloaded overlay packages
    https://android-review.googlesource.com/#/c/81931/7
    
    Make asset manager cookies opaque
    https://android-review.googlesource.com/#/c/187454/3
    
    Add ResTable::remove(cookie)
    https://android-review.googlesource.com/#/c/187455/3
    
    Support tagging resources as OK to overlay
    https://android-review.googlesource.com/#/c/187456/3
    
    OMS: introduce the OverlayManagerService
    https://android-review.googlesource.com/#/c/187457/3
    
    OMS: integrate OverlayManagerService into framework
    https://android-review.googlesource.com/#/c/187459/4
    
    PackageManager: set EXTRA_REPLACING correctly in ACTION_PACKAGE_ADDED
    https://android-review.googlesource.com/#/c/235091/
    
    idmap: suppress print for padded resources
    https://android-review.googlesource.com/#/c/235092/
    
    Check invariants after ResTable is modified
    https://android-review.googlesource.com/#/c/235093/
    
    Ensure unique type IDs when parsing overlays
    https://android-review.googlesource.com/#/c/235094/
    
    Add back assetSeq
    From patchset 3 of:
    OMS: integrate OverlayManagerService into framework
    Author Mårten Kongstad
    https://android-review.googlesource.com/#/c/187459/3
    
    Change-Id: I9335876e94fd264d9f3b91b5871e753bec448af2
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- cmds/idmap/create.cpp
+++ cmds/idmap/create.cpp
@@ -1,6 +1,6 @@
 #include "idmap.h"
 
-#include <UniquePtr.h>
+#include <memory>
 #include <androidfw/AssetManager.h>
 #include <androidfw/ResourceTypes.h>
 #include <androidfw/ZipFileRO.h>
@@ -15,7 +15,7 @@ using namespace android;
 namespace {
     int get_zip_entry_crc(const char *zip_path, const char *entry_name, uint32_t *crc)
     {
-        UniquePtr<ZipFileRO> zip(ZipFileRO::open(zip_path));
+        std::unique_ptr<ZipFileRO> zip(ZipFileRO::open(zip_path));
         if (zip.get() == NULL) {
             return -1;
         }
@@ -41,7 +41,7 @@ namespace {
             ALOGD("error: fchmod %s: %s\n", path, strerror(errno));
             goto fail;
         }
-        if (TEMP_FAILURE_RETRY(flock(fd, LOCK_EX | LOCK_NB)) != 0) {
+        if (TEMP_FAILURE_RETRY(flock(fd, LOCK_EX)) != 0) {
             ALOGD("error: flock %s: %s\n", path, strerror(errno));
             goto fail;
         }
@@ -106,7 +106,7 @@ fail:
 
         uint32_t cached_target_crc, cached_overlay_crc;
         String8 cached_target_path, cached_overlay_path;
-        if (!ResTable::getIdmapInfo(buf, N, NULL, &cached_target_crc, &cached_overlay_crc,
+        if (!ResTable::getIdmapInfo(buf, N, NULL, NULL, &cached_target_crc, &cached_overlay_crc,
                     &cached_target_path, &cached_overlay_path)) {
             return true;
         }
