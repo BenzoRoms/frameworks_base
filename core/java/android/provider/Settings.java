f7d5ed57037aaf11bd893eebe95c26494e0d6d33
rogersb11 <brettrogers11@gmail.com>
2016-07-04 22:34:09 +0200

    FWB: Volume Dialog: Force expanded mode 2/2
    Change-Id: I71c4cb260ae7f9ac859159e95671a3fe357b0ec4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5112,6 +5112,12 @@ public final class Settings {
         public static final String LOCKSCREEN_SECURITY_ALPHA = "lockscreen_security_alpha";
 
         /**
+         * Force Volume panel in expanded mode
+         * @hide
+         */
+        public static final String VOLUME_DIALOG_FORCE_EXPANDED = "volume_dialog_force_expanded";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
@@ -114,9 +114,11 @@ public class VolumeDialog {
     private final ColorStateList mActiveSliderTint;
     private final ColorStateList mInactiveSliderTint;
     private final VolumeDialogMotion mMotion;
+    private Context vContext;
 
     private boolean mShowing;
     private boolean mExpanded;
+    private boolean mForceExpanded;
     private int mActiveStream;
     private boolean mShowHeaders = VolumePrefs.DEFAULT_SHOW_HEADERS;
     private boolean mAutomute = VolumePrefs.DEFAULT_ENABLE_AUTOMUTE;
@@ -137,6 +139,7 @@ public class VolumeDialog {
     public VolumeDialog(Context context, int windowType, VolumeDialogController controller,
             ZenModeController zenModeController, Callback callback) {
         mContext = context;
+        vContext = context;
         mController = controller;
         mCallback = callback;
         mSpTexts = new SpTexts(mContext);
@@ -221,6 +224,7 @@ public class VolumeDialog {
 
         controller.addCallback(mControllerCallbackH, mHandler);
         controller.getState();
+        updateForceExpanded();
     }
 
     private ColorStateList loadColorStateList(int colorResId) {
@@ -341,6 +345,7 @@ public class VolumeDialog {
         writer.println(VolumeDialog.class.getSimpleName() + " state:");
         writer.print("  mShowing: "); writer.println(mShowing);
         writer.print("  mExpanded: "); writer.println(mExpanded);
+        writer.print("  mForceExpanded: "); writer.println(mForceExpanded);
         writer.print("  mExpandButtonAnimationRunning: ");
         writer.println(mExpandButtonAnimationRunning);
         writer.print("  mActiveStream: "); writer.println(mActiveStream);
@@ -481,7 +486,7 @@ public class VolumeDialog {
         if (mSafetyWarning != null) return 5000;
         int mVolumeDialogTimeout = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.VOLUME_DIALOG_TIMEOUT, 3000);
-        if (mExpanded || mExpandButtonAnimationRunning) return mVolumeDialogTimeout;
+        if (mExpanded || mForceExpanded) return mVolumeDialogTimeout;
         if (mActiveStream == AudioManager.STREAM_MUSIC) return mVolumeDialogTimeout;
         return mVolumeDialogTimeout;
     }
@@ -566,6 +571,8 @@ public class VolumeDialog {
 
     private void updateExpandButtonH() {
         if (D.BUG) Log.d(TAG, "updateExpandButtonH");
+        // TO DO: Make button unclickable when force expanded view, need to
+        // update "off" state for this to be done without causing ui issues
         mExpandButton.setClickable(!mExpandButtonAnimationRunning);
         if (mExpandButtonAnimationRunning && isAttached()) return;
         final int res = mExpanded ? R.drawable.ic_volume_collapse_animation
@@ -584,7 +591,7 @@ public class VolumeDialog {
         if (linkNotificationWithVolume && isNotificationStream) {
             return false;
         }
-        return mExpanded && row.view.getVisibility() == View.VISIBLE
+        return mExpanded || mForceExpanded && row.view.getVisibility() == View.VISIBLE
                 || (mExpanded && (row.important || isActive))
                 || !mExpanded && isActive;
     }
@@ -594,6 +601,7 @@ public class VolumeDialog {
         final VolumeRow activeRow = getActiveRow();
         updateFooterH();
         updateExpandButtonH();
+        updateForceExpanded();
         setVolumeAlpha();
         if (!mShowing) {
             trimObsoleteH();
@@ -782,7 +790,7 @@ public class VolumeDialog {
     }
 
     private void updateVolumeRowSliderTintH(VolumeRow row, boolean isActive) {
-        if (isActive && mExpanded) {
+        if (isActive && mForceExpanded) {
             row.slider.requestFocus();
         }
         final ColorStateList tint = isActive && row.slider.isEnabled() ? mActiveSliderTint
@@ -1175,5 +1183,10 @@ public class VolumeDialog {
         if (mDialogView != null) {
             mDialogView.getBackground().setAlpha(mVolumeDialogAlpha);
         }
+    }
+
+    private void updateForceExpanded() {
+        mForceExpanded = Settings.System.getInt(vContext.getContentResolver(),
+                Settings.System.VOLUME_DIALOG_FORCE_EXPANDED, 0) == 1;
     }
 }
