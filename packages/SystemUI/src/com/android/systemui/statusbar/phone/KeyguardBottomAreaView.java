e540e9baddde61064da36f7074f5b2d59524cd59
Alex Cruz <mazdarider23@gmail.com>
2016-07-04 21:53:26 +0200

    fix potential NPE's
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -341,11 +341,11 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         if (visible) {
             if (isTargetCustom(Shortcuts.LEFT_SHORTCUT)) {
                 visible = !mShortcutHelper.isTargetEmpty(Shortcuts.LEFT_SHORTCUT);
-            } else {
-                // El Janky lives again
             }
         }
-        mLeftAffordanceView.setVisibility(visible ? View.VISIBLE : View.GONE);
+        if (mLeftAffordanceView != null) {
+            mLeftAffordanceView.setVisibility(visible ? View.VISIBLE : View.GONE);
+        }
     }
 
     private void updateCameraVisibility() {
@@ -363,7 +363,9 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
                         && getResources().getBoolean(R.bool.config_keyguardShowCameraAffordance);
             }
         }
-        mCameraImageView.setVisibility(visible ? View.VISIBLE : View.GONE);
+        if (mCameraImageView != null) {
+            mCameraImageView.setVisibility(visible ? View.VISIBLE : View.GONE);
+        }
     }
 
     private void updateLeftAffordanceIcon() {
