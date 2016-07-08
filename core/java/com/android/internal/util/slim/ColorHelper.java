debb14e43b7fc1bb9f397f168a709b8edb13595f
mydongistiny <jaysonedson@gmail.com>
2016-07-08 15:43:07 -0700

    ColorHelper: Add some more functions
    Extra code taken from DarkKat
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/com/android/internal/util/slim/ColorHelper.java
+++ core/java/com/android/internal/util/slim/ColorHelper.java
@@ -19,6 +19,7 @@ package com.android.internal.util.slim;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
@@ -28,7 +29,20 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.VectorDrawable;
 import android.graphics.drawable.Drawable;
 
+import java.util.Random;
+
 public class ColorHelper {
+
+    public static int getBlendColor(int from, int to, float ratio) {
+        final float inverseRatio = 1f - ratio;
+
+        final float a = Color.alpha(to) * ratio + Color.alpha(from) * inverseRatio;
+        final float r = Color.red(to) * ratio + Color.red(from) * inverseRatio;
+        final float g = Color.green(to) * ratio + Color.green(from) * inverseRatio;
+        final float b = Color.blue(to) * ratio + Color.blue(from) * inverseRatio;
+
+        return Color.argb((int) a, (int) r, (int) g, (int) b);
+    }
 
     public static Drawable getColoredDrawable(Drawable d, int color) {
         if (d instanceof VectorDrawable) {
@@ -77,4 +91,84 @@ public class ColorHelper {
         return bmpGrayscale;
     }
 
+    public static boolean isColorDark(int color) {
+        double a = 1- (0.299 * Color.red(color)
+                + 0.587 * Color.green(color)
+                + 0.114 * Color.blue(color)) / 255;
+        if (a < 0.5) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static int getLightenOrDarkenColor(int originalColor) {
+        boolean isOriginalColorDark = isColorDark(originalColor);
+        float factor = isOriginalColorDark ? 0.1f : 0.8f;
+        int a = Color.alpha(originalColor);
+        int r = Color.red(originalColor);
+        int g = Color.green(originalColor);
+        int b = Color.blue(originalColor);
+        int newColor;
+
+        if (isOriginalColorDark) {
+            newColor = Color.argb(a,
+                    (int) ((r * (1 - factor) / 255 + factor) * 255),
+                    (int) ((g * (1 - factor) / 255 + factor) * 255),
+                    (int) ((b * (1 - factor) / 255 + factor) * 255));
+        } else {
+            newColor = Color.argb(a,
+                    Math.max((int) (r * factor), 0),
+                    Math.max((int) (g * factor), 0),
+                    Math.max((int) (b * factor), 0));
+        }
+        return newColor;
+    }
+
+    public static String convertToColorHexString(int color, boolean useAlpha) {
+        String alpha = Integer.toHexString(Color.alpha(color));
+        String red = Integer.toHexString(Color.red(color));
+        String green = Integer.toHexString(Color.green(color));
+        String blue = Integer.toHexString(Color.blue(color));
+
+        if (useAlpha) {
+            if (alpha.length() == 1) {
+                alpha = "0" + alpha;
+            }
+        }
+
+        if (red.length() == 1) {
+            red = "0" + red;
+        }
+
+        if (green.length() == 1) {
+            green = "0" + green;
+        }
+
+        if (blue.length() == 1) {
+            blue = "0" + blue;
+        }
+
+        return "#" + (useAlpha ? alpha : "") + red + green + blue;
+    }
+
+    public static int getRandomColor() {
+        Random rand = new Random();
+        return rand.nextInt(0xFFFFFF);
+    }
+
+    public static ColorMatrixColorFilter getColorFilter(int color) {
+        float r = Color.red(color) / 255f;
+        float g = Color.green(color) / 255f;
+        float b = Color.blue(color) / 255f;
+
+        ColorMatrix cm = new ColorMatrix(new float[] {
+                r, 0, 0, 0, 0,
+                0, g, 0, 0, 0,
+                0, 0, b, 0, 0,
+                0, 0, 0, 1, 0,
+        });
+        ColorMatrixColorFilter cf = new ColorMatrixColorFilter(cm);
+        return cf;
+    }
 }
