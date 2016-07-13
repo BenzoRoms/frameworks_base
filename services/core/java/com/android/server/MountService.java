a5bcb9ff921218c56f4d244a25a1efaf1531b844
yuanhuihui <yuanhuihui@xiaomi.com>
2016-07-13 21:21:03 +0800

    fixes mStartedUsers incorrect locked by mLock or mVolumes
    when create/delete multiple user, plug/unplug externel storage(such as sdcard) at the same time.
    multiple threads run concurrency , the newly created user can not get state of externel storage.
    
    https://code.google.com/p/android/issues/detail?id=215901
    
    Change-Id: I097f5291108af89a430e12045376065d71d48175
    Signed-off-by: yuanhuihui <yuanhuihui@xiaomi.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/MountService.java
+++ services/core/java/com/android/server/MountService.java
@@ -824,7 +824,7 @@ class MountService extends IMountService.Stub
 
         // Record user as started so newly mounted volumes kick off events
         // correctly, then synthesize events for any already-mounted volumes.
-        synchronized (mVolumes) {
+        synchronized (mLock) {
             for (int i = 0; i < mVolumes.size(); i++) {
                 final VolumeInfo vol = mVolumes.valueAt(i);
                 if (vol.isVisibleForRead(userId) && vol.isMountedReadable()) {
@@ -847,7 +847,7 @@ class MountService extends IMountService.Stub
         } catch (NativeDaemonConnectorException ignored) {
         }
 
-        synchronized (mVolumes) {
+        synchronized (mLock) {
             mStartedUsers = ArrayUtils.removeInt(mStartedUsers, userId);
         }
     }
