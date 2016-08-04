AOSPA/android_frameworks_base-old

[1/2] Frameworks: HALO
Thanks @beerbong for kk port.

* Key contributors:
@drcmda
@beerbong
@thecrazyskull
@carlosavignano
@Anu6is

* If HALO is enabled, headsup will be disabled

Change-Id: I8dc6a5a52fe4688b60cbec75e9a12e6f299e1d6d
Signed-off-by: thecrazyskull <anaskarbila@aospa.co>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -87,6 +87,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     private View mSignalCluster;
     private SettingsButton mSettingsButton;
     private View mSettingsContainer;
+    private View mHaloButton;
     private QsAddButton mQsAddButton;
     private View mQsDetailHeader;
     private TextView mQsDetailHeaderTitle;
@@ -171,6 +172,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mSettingsButton = (SettingsButton) findViewById(R.id.settings_button);
         mSettingsContainer = findViewById(R.id.settings_button_container);
         mSettingsButton.setOnClickListener(this);
+        mHaloButton = findViewById(R.id.halo_button);
+        mHaloButton.setOnClickListener(this);
         mQsAddButton = (QsAddButton) findViewById(R.id.qs_add_button);
         mQsAddButton.setOnClickListener(this);
         mQsDetailHeader = findViewById(R.id.qs_detail_header);
@@ -280,6 +283,10 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             ((RippleDrawable) d).setForceSoftware(true);
         }
         d = mQsAddButton.getBackground();
+        if (d instanceof RippleDrawable) {
+            ((RippleDrawable) d).setForceSoftware(true);
+        }
+        d = mHaloButton.getBackground();
         if (d instanceof RippleDrawable) {
             ((RippleDrawable) d).setForceSoftware(true);
         }
@@ -436,6 +443,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mDateExpanded.setVisibility(mExpanded && mAlarmShowing ? View.INVISIBLE : View.VISIBLE);
         mAlarmStatus.setVisibility(mExpanded && mAlarmShowing ? View.VISIBLE : View.INVISIBLE);
         mSettingsContainer.setVisibility(mExpanded ? View.VISIBLE : View.INVISIBLE);
+        mHaloButton.setVisibility(mExpanded ? View.VISIBLE : View.INVISIBLE);
         mQsAddButton.setVisibility(mExpanded && mQsAbleToShowHidden
                 ? View.VISIBLE : View.INVISIBLE);
         mQsDetailHeader.setVisibility(mExpanded && mShowingDetail ? View.VISIBLE : View.INVISIBLE);
@@ -469,9 +477,10 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
 
     private void updateSystemIconsLayoutParams() {
         RelativeLayout.LayoutParams lp = (LayoutParams) mSystemIconsSuperContainer.getLayoutParams();
+        int baseId = mHaloButton != null ? mHaloButton.getId() : mQsAddButton.getId();
         int rule = mExpanded
                 ? mQsAbleToShowHidden
-                        ? mQsAddButton.getId()
+                        ? baseId
                         : mSettingsContainer.getId()
                 : mMultiUserSwitch.getId();
         if (rule != lp.getRules()[RelativeLayout.START_OF]) {
@@ -639,6 +648,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             startClockActivity();
         } else if (v == mDateGroup) {
             startDateActivity();
+        } else if (v == mHaloButton) {
+            toggleHalo();
         }
     }
 
@@ -720,6 +731,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         target.settingsTranslation = mExpanded
                 ? 0
                 : mMultiUserSwitch.getLeft() - mSettingsContainer.getLeft();
+        target.haloButtonAlpha = getAlphaForVisibility(mHaloButton);
+        target.haloButtonTranslation = mExpanded ? 0 : mSettingsContainer.getLeft() - mHaloButton.getLeft();
         target.qsAddAlpha = getAlphaForVisibility(mQsAddButton);
         target.qsAddTranslation = mExpanded
                 ? 0
@@ -784,6 +797,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mQsAddButton.setTranslationY(mSystemIconsSuperContainer.getTranslationY());
         mQsAddButton.setTranslationX(values.qsAddTranslation);
         mQsAddButton.setRotation(values.qsAddRotation);
+        mHaloButton.setTranslationX(values.haloButtonTranslation);
         applyAlpha(mEmergencyCallsOnly, values.emergencyCallsOnlyAlpha);
         if (!mShowingDetail && !mDetailTransitioning) {
             // Otherwise it needs to stay invisible
@@ -793,6 +807,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         applyAlpha(mDateExpanded, values.dateExpandedAlpha);
         applyAlpha(mBatteryLevel, values.batteryLevelAlpha);
         applyAlpha(mSettingsContainer, values.settingsAlpha);
+        applyAlpha(mHaloButton, values.haloButtonAlpha);
         applyAlpha(mQsAddButton, values.qsAddAlpha);
         applyAlpha(mSignalCluster, values.signalClusterAlpha);
         if (!mExpanded) {
@@ -824,6 +839,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         float batteryLevelExpandedAlpha;
         float settingsAlpha;
         float settingsTranslation;
+        float haloButtonAlpha;
+        float haloButtonTranslation;
         float qsAddAlpha;
         float qsAddTranslation;
         float signalClusterAlpha;
@@ -840,6 +857,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             batteryX = v1.batteryX * (1 - t) + v2.batteryX * t;
             batteryY = v1.batteryY * (1 - t) + v2.batteryY * t;
             settingsTranslation = v1.settingsTranslation * (1 - t) + v2.settingsTranslation * t;
+            haloButtonTranslation = v1.haloButtonTranslation * (1 -t) + v2.haloButtonTranslation * t;
             qsAddTranslation = v1.qsAddTranslation * (1 - t) + v2.qsAddTranslation * t;
 
             float t1 = Math.max(0, t - 0.5f) * 2;
@@ -854,6 +872,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             float t3 = Math.max(0, t - 0.7f) / 0.3f;
             batteryLevelAlpha = v1.batteryLevelAlpha * (1 - t3) + v2.batteryLevelAlpha * t3;
             settingsAlpha = v1.settingsAlpha * (1 - t3) + v2.settingsAlpha * t3;
+            haloButtonAlpha = v1.haloButtonAlpha * (1 - t3) + v2.haloButtonAlpha * t3;
             qsAddAlpha = v1.qsAddAlpha * (1 - t3) + v2.qsAddAlpha * t3;
             dateExpandedAlpha = v1.dateExpandedAlpha * (1 - t3) + v2.dateExpandedAlpha * t3;
             dateCollapsedAlpha = v1.dateCollapsedAlpha * (1 - t3) + v2.dateCollapsedAlpha * t3;
@@ -1020,6 +1039,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             transition(mBatteryMeterView, !isInReorderMode);
             transition(mBatteryLevel, !isInReorderMode);
             transition(mQsDeleteHeader, isInReorderMode);
+            transition(mHaloButton, !isInReorderMode);
             if (isInReorderMode) {
                 mQsDeleteHeader.getDrawable().setAlpha(0xFF);
             }
@@ -1048,6 +1068,11 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                     .start();
         }
     };
+
+    private boolean toggleHalo() {
+        return Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_ACTIVE, 1);
+    }
 
     class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {

services/core/java/com/android/server/notification/RankingHelper.java
@@ -52,12 +52,14 @@ public class RankingHelper implements RankingConfig {
     private static final String ATT_PEEKABLE = "peekable";
     private static final String ATT_VISIBILITY = "visibility";
     private static final String ATT_FLOATING = "floating";
+    private static final String ATT_HALO = "halo";
 
     private static final int DEFAULT_PRIORITY = Notification.PRIORITY_DEFAULT;
     private static final boolean DEFAULT_PEEKABLE = true;
     private static final int DEFAULT_VISIBILITY =
             NotificationListenerService.Ranking.VISIBILITY_NO_OVERRIDE;
     private static final boolean DEFAULT_FLOATING = true;
+    private static final boolean DEFAULT_HALO = true;
 
     private final NotificationSignalExtractor[] mSignalExtractors;
     private final NotificationComparator mPreliminaryComparator = new NotificationComparator();
@@ -146,6 +148,8 @@ public class RankingHelper implements RankingConfig {
                     boolean peekable = safeBool(parser, ATT_PEEKABLE, DEFAULT_PEEKABLE);
                     int vis = safeInt(parser, ATT_VISIBILITY, DEFAULT_VISIBILITY);
                     boolean floating = safeBool(parser, ATT_FLOATING, DEFAULT_FLOATING);
+                    boolean halo = safeBool(parser, ATT_HALO, DEFAULT_HALO);
+
                     String name = parser.getAttributeValue(null, ATT_NAME);
 
                     if (!TextUtils.isEmpty(name)) {
@@ -178,6 +182,9 @@ public class RankingHelper implements RankingConfig {
                         if (floating != DEFAULT_FLOATING) {
                             r.floating = floating;
                         }
+                        if (halo != DEFAULT_HALO) {
+                            r.halo = halo;
+                        }
                     }
                 }
             }
@@ -206,7 +213,8 @@ public class RankingHelper implements RankingConfig {
         for (int i = N - 1; i >= 0; i--) {
             final Record r = mRecords.valueAt(i);
             if (r.priority == DEFAULT_PRIORITY && r.peekable == DEFAULT_PEEKABLE
-                    && r.visibility == DEFAULT_VISIBILITY && r.floating == DEFAULT_FLOATING) {
+                    && r.visibility == DEFAULT_VISIBILITY && r.floating == DEFAULT_FLOATING
+                            && r.halo == DEFAULT_HALO) {
                 mRecords.remove(i);
             }
         }
@@ -235,6 +243,9 @@ public class RankingHelper implements RankingConfig {
             }
             if (r.floating != DEFAULT_FLOATING) {
                 out.attribute(null, ATT_FLOATING, Boolean.toString(r.floating));
+            }
+            if (r.halo != DEFAULT_HALO) {
+                out.attribute(null, ATT_HALO, Boolean.toString(r.halo));
             }
             if (!forBackup) {
                 out.attribute(null, ATT_UID, Integer.toString(r.uid));
@@ -402,6 +413,22 @@ public class RankingHelper implements RankingConfig {
         updateConfig();
     }
 
+    @Override
+    public boolean isPackageAllowedForHalo(String packageName, int uid) {
+        final Record r = mRecords.get(recordKey(packageName, uid));
+        return r != null ? r.halo : DEFAULT_HALO;
+    }
+
+    @Override
+    public void setHaloPolicyBlack(String packageName, int uid, boolean halo) {
+        if (halo == isPackageAllowedForHalo(packageName, uid)) {
+            return;
+        }
+        getOrCreateRecord(packageName, uid).halo = halo;
+        removeDefaultRecords();
+        updateConfig();
+    }
+
     public void dump(PrintWriter pw, String prefix, NotificationManagerService.DumpFilter filter) {
         if (filter == null) {
             final int N = mSignalExtractors.length;
@@ -450,6 +477,10 @@ public class RankingHelper implements RankingConfig {
                     pw.print("floating=");
                     pw.print(r.floating);
                 }
+                if (r.halo != DEFAULT_HALO) {
+                    pw.print("halo=");
+                    pw.print(r.halo);
+                }
                 pw.println();
             }
         }
@@ -489,6 +520,7 @@ public class RankingHelper implements RankingConfig {
         boolean peekable = DEFAULT_PEEKABLE;
         int visibility = DEFAULT_VISIBILITY;
         boolean floating = DEFAULT_FLOATING;
+        boolean halo = DEFAULT_HALO;
     }
 
 }

BenzoRoms/frameworks_base

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -113,6 +113,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     private LinearLayout mSystemIcons;
     private View mSignalCluster;
     private View mSettingsButton;
+    private View mHaloButton;
     private View mSomcQuickSettings;
     private View mQsDetailHeader;
     private TextView mQsDetailHeaderTitle;
@@ -232,6 +233,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mSettingsButton = findViewById(R.id.settings_button);
         mSettingsButton.setOnClickListener(this);
         mSettingsButton.setOnLongClickListener(this);
+        mHaloButton = findViewById(R.id.halo_button);
+        mHaloButton.setOnClickListener(this);
         mHeadsUpButton = findViewById(R.id.heads_up_button);
         mHeadsUpButton.setOnClickListener(this);
         mHeadsUpButton.setOnLongClickListener(this);
@@ -285,6 +288,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         // settings), so disable it for this view
         ((RippleDrawable) getBackground()).setForceSoftware(true);
         ((RippleDrawable) mSettingsButton.getBackground()).setForceSoftware(true);
+        ((RippleDrawable) mHaloButton.getBackground()).setForceSoftware(true);
         ((RippleDrawable) mSystemIconsSuperContainer.getBackground()).setForceSoftware(true);
 
         mWeatherClient = new OmniJawsClient(mContext);
@@ -439,6 +443,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             mDateExpanded.setVisibility(mExpanded && mAlarmShowing ? View.INVISIBLE : View.VISIBLE);
             mAlarmStatus.setVisibility(mExpanded && mAlarmShowing ? View.VISIBLE : View.INVISIBLE);
             mSettingsButton.setVisibility(mExpanded ? View.VISIBLE : View.INVISIBLE);
+            mHaloButton.setVisibility(mExpanded ? View.VISIBLE : View.INVISIBLE);
             mQsDetailHeader.setVisibility(mExpanded && mShowingDetail ? View.VISIBLE : View.INVISIBLE);
             if (mHeadsUpButton != null) {
                 mHeadsUpButton.setVisibility(mExpanded && mShowHeadsUpButton ? View.VISIBLE : View.GONE);
@@ -472,15 +477,20 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
 
     private void updateSystemIconsLayoutParams() {
         RelativeLayout.LayoutParams lp = (LayoutParams) mSystemIconsSuperContainer.getLayoutParams();
+        int halo = mHaloButton != null 
+                ? mHaloButton.getId() : mSettingsButton.getId();
         int headsUp = mHeadsUpButton != null
-                ? mHeadsUpButton.getId() : mSettingsButton.getId();
+                ? mHeadsUpButton.getId() : halo;
         int quicksb = mSomcQuickSettings.getVisibility() != View.GONE
                 ? mSomcQuickSettings.getId() : headsUp;
+        int ruleHalo = mExpanded
+                ? halo
+                : mMultiUserSwitch.getId();
         int ruleHeadsup = mExpanded
-                ? headsUp
+                ? halo
                 : mMultiUserSwitch.getId();
         int ruleQuickSB = mExpanded
-                ? quicksb
+                ? headsUp
                 : mMultiUserSwitch.getId();
         if (ruleHeadsup != lp.getRules()[RelativeLayout.START_OF] &&
                 ruleQuickSB != lp.getRules()[RelativeLayout.START_OF]) {
@@ -658,6 +668,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             startClockActivity();
         } else if (v == mDateGroup) {
             startDateActivity();
+        } else if (v == mHaloButton) {
+            toggleHalo();
         } else if (v == mWeatherImage) {
             try {
                 if (!mWeatherDataInvalid && mWeatherData != null) {
@@ -877,11 +889,15 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         target.settingsTranslation = mExpanded
                 ? 0
                 : mMultiUserSwitch.getLeft() - mSettingsButton.getLeft();
+        target.haloButtonAlpha = getAlphaForVisibility(mHaloButton);
+        target.haloButtonTranslation = mExpanded
+                ? 0
+                : mSettingsButton.getLeft() - mHaloButton.getLeft();
         target.headsUpAlpha = getAlphaForVisibility(mHeadsUpButton);
         if (mHeadsUpButton != null) {
             target.headsUpTranslation = mExpanded
                      ? 0
-                     : mSettingsButton.getLeft() - mHeadsUpButton.getLeft();
+                     : mHaloButton.getLeft() - mHeadsUpButton.getLeft();
              target.settingsRotation = !mExpanded ? 90f : 0f;
         }
         target.somcQuickSettingsAlpha = getAlphaForVisibility(mSomcQuickSettings);
@@ -951,6 +967,9 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mSettingsButton.setTranslationY(mSystemIconsSuperContainer.getTranslationY());
         mSettingsButton.setTranslationX(values.settingsTranslation);
         mSettingsButton.setRotation(values.settingsRotation);
+        mHaloButton.setTranslationY(mSystemIconsSuperContainer.getTranslationY());
+        mHaloButton.setTranslationX(values.headsUpTranslation);
+        mHaloButton.setRotation(values.settingsRotation);
         if (mHeadsUpButton != null) {
             mHeadsUpButton.setTranslationY(mSystemIconsSuperContainer.getTranslationY());
             mHeadsUpButton.setTranslationX(values.headsUpTranslation);
@@ -969,6 +988,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         applyAlpha(mDateExpanded, values.dateExpandedAlpha);
         applyAlpha(mBatteryLevel, values.batteryLevelAlpha);
         applyAlpha(mSettingsButton, values.settingsAlpha);
+        applyAlpha(mHaloButton, values.haloButtonAlpha);
         applyAlpha(mHeadsUpButton, values.headsUpAlpha);
         applyAlpha(mSomcQuickSettings, values.settingsAlpha);
         applyAlpha(mSignalCluster, values.signalClusterAlpha);
@@ -1001,6 +1021,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         float batteryLevelExpandedAlpha;
         float settingsAlpha;
         float settingsTranslation;
+        float haloButtonAlpha;
+        float haloButtonTranslation;
         float signalClusterAlpha;
         float settingsRotation;
         float headsUpAlpha;
@@ -1019,6 +1041,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             batteryX = v1.batteryX * (1 - t) + v2.batteryX * t;
             batteryY = v1.batteryY * (1 - t) + v2.batteryY * t;
             settingsTranslation = v1.settingsTranslation * (1 - t) + v2.settingsTranslation * t;
+            haloButtonTranslation =
+                    v1.haloButtonTranslation * (1 -t) + v2.haloButtonTranslation * t;
             headsUpTranslation =
                     v1.headsUpTranslation * (1 - t) + v2.headsUpTranslation * t;
             somcQuickSettingsTranslation =
@@ -1035,6 +1059,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             float t3 = Math.max(0, t - 0.7f) / 0.3f;
             batteryLevelAlpha = v1.batteryLevelAlpha * (1 - t3) + v2.batteryLevelAlpha * t3;
             settingsAlpha = v1.settingsAlpha * (1 - t3) + v2.settingsAlpha * t3;
+            haloButtonAlpha = v1.haloButtonAlpha * (1 - t3) + v2.haloButtonAlpha * t3;
             headsUpAlpha = v1.headsUpAlpha * (1 - t3) + v2.headsUpAlpha * t3;
             somcQuickSettingsAlpha =
                     v1.somcQuickSettingsAlpha * (1 - t3) + v2.somcQuickSettingsAlpha * t3;
@@ -1139,6 +1164,11 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             }
         }
     };
+
+    private boolean toggleHalo() {
+        return Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.HALO_ACTIVE, 1);
+    }
 
     class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {

services/core/java/com/android/server/notification/RankingHelper.java
@@ -52,6 +52,7 @@ public class RankingHelper implements RankingConfig {
     private static final String ATT_PEEKABLE = "peekable";
     private static final String ATT_VISIBILITY = "visibility";
     private static final String ATT_KEYGUARD = "keyguard";
+    private static final String ATT_HALO = "halo";
 
     private static final int DEFAULT_PRIORITY = Notification.PRIORITY_DEFAULT;
     private static final boolean DEFAULT_PEEKABLE = true;
@@ -61,6 +62,7 @@ public class RankingHelper implements RankingConfig {
             // By default, show only non ongoing notifications on lockscreen
             Notification.SHOW_ALL_NOTI_ON_KEYGUARD
                     + Notification.SHOW_NO_ONGOING_NOTI_ON_KEYGUARD;
+    private static final boolean DEFAULT_HALO = true;
 
     private final NotificationSignalExtractor[] mSignalExtractors;
     private final NotificationComparator mPreliminaryComparator = new NotificationComparator();
@@ -149,6 +151,7 @@ public class RankingHelper implements RankingConfig {
                     boolean peekable = safeBool(parser, ATT_PEEKABLE, DEFAULT_PEEKABLE);
                     int vis = safeInt(parser, ATT_VISIBILITY, DEFAULT_VISIBILITY);
                     int keyguard = safeInt(parser, ATT_KEYGUARD, DEFAULT_KEYGUARD);
+                    boolean halo = safeBool(parser, ATT_HALO, DEFAULT_HALO);
                     String name = parser.getAttributeValue(null, ATT_NAME);
 
                     if (!TextUtils.isEmpty(name)) {
@@ -181,6 +184,9 @@ public class RankingHelper implements RankingConfig {
                         if (keyguard != DEFAULT_KEYGUARD) {
                             r.keyguard = keyguard;
                         }
+                        if (halo != DEFAULT_HALO) {
+                            r.halo = halo;
+                        }
                     }
                 }
             }
@@ -209,8 +215,9 @@ public class RankingHelper implements RankingConfig {
         for (int i = N - 1; i >= 0; i--) {
             final Record r = mRecords.valueAt(i);
             if (r.priority == DEFAULT_PRIORITY && r.peekable == DEFAULT_PEEKABLE
-                    && r.visibility == DEFAULT_VISIBILITY && r.keyguard == DEFAULT_KEYGUARD) {
-                mRecords.removeAt(i);
+                    && r.visibility == DEFAULT_VISIBILITY && r.keyguard == DEFAULT_KEYGUARD
+                            && r.halo == DEFAULT_HALO) {
+                mRecords.remove(i);
             }
         }
     }
@@ -238,6 +245,9 @@ public class RankingHelper implements RankingConfig {
             }
             if (r.keyguard != DEFAULT_KEYGUARD) {
                 out.attribute(null, ATT_KEYGUARD, Integer.toString(r.keyguard));
+            }
+            if (r.halo != DEFAULT_HALO) {
+                out.attribute(null, ATT_HALO, Boolean.toString(r.halo));
             }
             if (!forBackup) {
                 out.attribute(null, ATT_UID, Integer.toString(r.uid));
@@ -405,6 +415,22 @@ public class RankingHelper implements RankingConfig {
         updateConfig();
     }
 
+    @Override
+    public boolean isPackageAllowedForHalo(String packageName, int uid) {
+        final Record r = mRecords.get(recordKey(packageName, uid));
+        return r != null ? r.halo : DEFAULT_HALO;
+    }
+
+    @Override
+    public void setHaloPolicyBlack(String packageName, int uid, boolean halo) {
+        if (halo == isPackageAllowedForHalo(packageName, uid)) {
+            return;
+        }
+        getOrCreateRecord(packageName, uid).halo = halo;
+        removeDefaultRecords();
+        updateConfig();
+    }
+
     public void dump(PrintWriter pw, String prefix, NotificationManagerService.DumpFilter filter) {
         if (filter == null) {
             final int N = mSignalExtractors.length;
@@ -453,6 +479,10 @@ public class RankingHelper implements RankingConfig {
                     pw.print(" keyguard=");
                     pw.print(r.keyguard);
                 }
+                if (r.halo != DEFAULT_HALO) {
+                    pw.print("halo=");
+                    pw.print(r.halo);
+                }
                 pw.println();
             }
         }
@@ -492,6 +522,7 @@ public class RankingHelper implements RankingConfig {
         boolean peekable = DEFAULT_PEEKABLE;
         int visibility = DEFAULT_VISIBILITY;
         int keyguard = DEFAULT_KEYGUARD;
+        boolean halo = DEFAULT_HALO;
     }
 
 }