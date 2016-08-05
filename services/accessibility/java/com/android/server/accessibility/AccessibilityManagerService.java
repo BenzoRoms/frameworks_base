Neoteric-OS/android_frameworks_base

Assuring unlinkToDeath only called on bound services.
This issue will cause user to experience fatal exception on system
server. Crash seen to occur at least once during full-dataset restore.

When a service that is connected dies unexpectedly it will trigger a
RemoteException when attempting to call IBinder.linkToDeath. This
dead binder will later cause cleanup code to run in binderDied which
will in turn lead to IBinder.unlinkToDeath being called for IBinder
that failed to linkToDeath.

Resolved by assuring that unlinkToDeath is only called on bound services
by checking that they were added to tracked array mBoundServices after
being linked properly before removing and doing unlinkToDeath.

Change-Id: I3d4d0063a96d18402cf92bf12e72680cadd66c6a

services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
@@ -1054,9 +1054,10 @@ public class AccessibilityManagerService extends IAccessibilityManager.Stub {
      * @param service The service.
      */
     private void removeServiceLocked(Service service, UserState userState) {
-        userState.mBoundServices.remove(service);
         userState.mComponentNameToServiceMap.remove(service.mComponentName);
-        service.unlinkToOwnDeathLocked();
+        if (userState.mBoundServices.remove(service)) {
+            service.unlinkToOwnDeathLocked();
+        }
     }
 
     /**

BenzoRoms/frameworks_base

Bug: 29525711
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
@@ -1079,9 +1079,10 @@ public class AccessibilityManagerService extends IAccessibilityManager.Stub {
      * @param service The service.
      */
     private void removeServiceLocked(Service service, UserState userState) {
-        userState.mBoundServices.remove(service);
         userState.mComponentNameToServiceMap.remove(service.mComponentName);
-        service.onRemoved();
+        if (userState.mBoundServices.remove(service)) {
+            service.onRemoved();
+        }
     }
 
     /**