bf4b95389e058bab9a458a08c9bca94690c8eb1d
Nicholas Chum <nicholaschum@gmail.com>
2016-08-30 01:23:42 -0400

    OMS-N: Officially announce the rebase completion to Nougat [14/14]
    This commit cleans up the possible issues with the current iteration
    of the Overlay Manager Service and it's free-compatibility with the
    Android Open Source Project based ROMs.
    
    This commit cleans up the commit to support downloaded overlays, but
    adapted to Android Nougat's new code.
    
    NOTE: enforceCodePolicy() has been added in Nougat to enforce checks on
    hasCode manifest metadata tags to ensure that APKs must declare that it
    wants an APK to be installed with no specified code. However this is
    rather harmful to us since our overlay APKs install as non-code based
    packages, thus tripping up the enforcement, ultimately requiring to be
    disabled.
    
    Change-Id: I077a11c155b41c1463d2cabdf7dcecd63d038620
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19112,6 +19112,10 @@ public final class ActivityManagerService extends ActivityManagerNative
                 }
             } catch (Exception e) {}
         }
+
+        Configuration config = new Configuration(mConfiguration);
+        config.assetSeq++;
+        updateConfiguration(config);
     }
 
     /**
