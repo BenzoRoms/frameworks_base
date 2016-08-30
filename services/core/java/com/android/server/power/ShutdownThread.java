0173e1212ea8c31bc1d008156bae5bd0a1446d0a
mydongistiny <jaysonedson@gmail.com>
2016-08-29 18:56:54 -0700

    DayNight: Fixups and color changes
    Includes:
    
    DayNight: Darker SystemUI theming
    https://github.com/BenzoRoms/frameworks_base/commit/d1ba3ff8614d580a81dea92a4d63e5d5ff185bb4
    
    DayNight: Dialogs and Power Menu Fixes
    https://github.com/BenzoRoms/frameworks_base/commit/f3978cb3d8eab1f77bd64681e4f2d204924e5856
    
    Dark Notifications
    https://github.com/BenzoRoms/frameworks_base/commit/687fed3e2afc8f32c39b852b09a1bef4e897d574
    
    Frameworks - DayNight notification actions/global actions update
    https://github.com/BenzoRoms/frameworks_base/commit/6854b69bae998e1609dca38b8f4299db78054e39
    
    DayNight: Invert screenshot notifications
    https://github.com/BenzoRoms/frameworks_base/commit/bf548c805f6d056308509cf96f02b2b5d215f4a7
    
    Frameworks - DayNight ambient display support
    https://github.com/BenzoRoms/frameworks_base/commit/aa09354f4ed885e4406eafbd47acb0bc16bcc9c1
    
    DayNight: Invert screenshot notifications
    https://github.com/BenzoRoms/frameworks_base/commit/bf548c805f6d056308509cf96f02b2b5d215f4a7
    
    DayNight: Accent Alarm and Media Icons
    https://github.com/BenzoRoms/frameworks_base/commit/64698e5d0c593d5ed5fd95d6913ad49fbc8aa6fb
    
    Set Teal 200 to the same value as Teal 500
    https://github.com/BenzoRoms/frameworks_base/commit/7ae32a586a3580de7a5a17b96007026b02363e4d
    
    Change-Id: Iab428e4eb117ec1f7a86dc4d17d9432805eea39b
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -187,7 +187,7 @@ public final class ShutdownThread extends Thread {
                 sConfirmDialog.dismiss();
                 sConfirmDialog = null;
             }
-            AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(context)
+            AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(context, com.android.internal.R.style.Theme_Material_DayNight_Dialog_Alert)
                     .setTitle(mRebootSafeMode
                             ? com.android.internal.R.string.reboot_safemode_title
                             : showRebootOption
@@ -320,7 +320,7 @@ public final class ShutdownThread extends Thread {
         }
 
         // Throw up a system dialog to indicate the device is rebooting / shutting down.
-        ProgressDialog pd = new ProgressDialog(context);
+        ProgressDialog pd = new ProgressDialog(context, com.android.internal.R.style.Theme_Material_DayNight_Dialog_Alert);
 
         // Path 1: Reboot to recovery for update
         //   Condition: mRebootReason == REBOOT_RECOVERY_UPDATE
