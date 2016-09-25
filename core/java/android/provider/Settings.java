850151843bd0ac095f4aebd1a86b04213d25fd6f
BeansTown106 <nbenis106@gmail.com>
2016-09-24 19:58:49 -0700

    QS: Add toggle for fancy QS animation [1/2]
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4413,6 +4413,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
 
         /**
+         * Quick Settings nougat animation
+         * @hide
+         */
+        public static final String QS_FANCY_ANIMATION = "qs_fancy_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
