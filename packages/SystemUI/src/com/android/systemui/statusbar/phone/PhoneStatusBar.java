c241eaaa08c60c04bc2864eb18622b3d9e5c2eb7
LorDClockaN <lordclockan@gmail.com>
2016-10-09 22:35:06 +0200

    Show4G: No need for SystemUI restart anymore
    Change-Id: Iff4cf0b649e6088496e32f66f93f7b76c0c66231
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -550,11 +550,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                             mContext.getContentResolver(),
                             Settings.System.SHOW_FOURG,
                             0, UserHandle.USER_CURRENT) == 1;
-                            Helpers.restartSystemUI();
-                            updateRowStates();
-                            updateSpeedbump();
-                            updateClearAll();
-                            updateEmptyShadeView();
+                    mNetworkController.onConfigurationChanged();
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_BENZO_LOGO_COLOR))) {
                 updateStatusBarLogoColor(true);
