8a9de4f78ec8db9940a105970b60727e0b7d04e2
g4s8 <g4s8.public@gmail.com>
2016-10-18 09:11:03 +0400

    frameworks: ScrollView.SavedState toString fix
    Corrected toString message of ScrollView.SavedState
    
    Test: see ScrollView.SavedState message
    
    Change-Id: I08a48f02f6ea9a72e756fc8260ce2e82b829ffb2
    Signed-off-by: g4s8 <g4s8.public@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/widget/ScrollView.java
+++ core/java/android/widget/ScrollView.java
@@ -1866,7 +1866,7 @@ public class ScrollView extends FrameLayout {
 
         @Override
         public String toString() {
-            return "HorizontalScrollView.SavedState{"
+            return "ScrollView.SavedState{"
                     + Integer.toHexString(System.identityHashCode(this))
                     + " scrollPosition=" + scrollPosition + "}";
         }
