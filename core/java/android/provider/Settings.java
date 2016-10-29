6a1d95fd5fe3670d7767c7bb192d53be168fbd61
beanstown106 <nbenis106@gmail.com>
2016-10-29 02:14:58 -0400

    Add ability to disable ripple when entering lockscreen pin [1/2]
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4799,6 +4799,12 @@ public final class Settings {
         public static final String SCREENSHOT_CROP_BEHAVIOR = "screenshot_crop_behavior";
 
         /**
+         * Whether to disable ripple on lockscreen pin entry
+         * @hide
+         */
+        public static final String LOCKSCREEN_PIN_RIPPLE = "lockscreen_pin_ripple";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
