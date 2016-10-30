33dee14400f97a3fdddb5253e426542521d1631f
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-30 20:04:25 +0100

    frameworks: show charging current & voltage on lockscreen [2/2]
    Thanx to ezio84 for the switch ;)
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    ezio 31/10/16: enabled by default
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4076,6 +4076,12 @@ public final class Settings {
          */
         public static final String BATTERY_BAR_LOCATION = "battery_bar_location";
 
+          /**
+          * Whether to show the negociated charger current in the lockscreen
+          * @hide
+          */
+         public static final String LOCKSCREEN_CHARGING_CURRENT = "lockscreen_charging_current";
+ 
         /**
          * @hide
          */
