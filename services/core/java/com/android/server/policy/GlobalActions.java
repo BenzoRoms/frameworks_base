e0a43ff7a3619efb91f36b6e172d624813a4c6c7
mydongistiny <jaysonedson@gmail.com>
2016-11-01 20:06:05 -0700

    OMS: Squash remove of all previous OMS commits
    Revert "N-Extras: Add dynamic theme BootAnimation support"
    
    This reverts commit c0c98cb9b597147476d7ab7ae0b998dbb935b46e.
    
    Revert "N-Extras: AudioService: Allow system effect sounds to be themed"
    
    This reverts commit 615ee349295e73316f58daca35ed000f54141c55.
    
    Revert "OMS-N: Small adjustment to Font commit"
    
    This reverts commit ab04531d8655e5b89c07625df52612d2b30a53ff.
    
    Revert "N-Extras: Add dynamic theme fonts support (Squash)"
    
    This reverts commit d724628f6d7e6bc3c4875da2f2a63d6cbe62c82f.
    
    Revert "OMS-N: ApplicationsState: add filter for Substratum overlays [1/2]"
    
    This reverts commit 52f99f5c72d0203e54603afb68ce3ae5680d224f.
    
    Revert "OMS-N: Allow on-demand window refreshes using "om refresh""
    
    This reverts commit ea124ef354a2d98176bb6d39e432f55c69077f87.
    
    Revert "OMS-N: Allow multiple targets for enabling and disabling overlays"
    
    This reverts commit 4debc52abfc748f8e885bdb4d49ae957a2d4fb8b.
    
    Revert "OMS-N: Disable Zygote preloaded drawables."
    
    This reverts commit 4e4eee3f3c3cdbd9f78c1d7f5c0b9cfed2b5ef94.
    
    Revert "OMS-N: Themes should be dynamically theming the power menu"
    
    This reverts commit 59d459e422fcd5b12bf080c81820d62d6a7c7db3.
    
    Revert "OMS-N: Officially announce the rebase completion to Nougat [14/14]"
    
    This reverts commit bf4b95389e058bab9a458a08c9bca94690c8eb1d.
    
    Revert "OMS-N: adb shell command to access OverlayManagerService [13/14]"
    
    This reverts commit d84c6069461cf5a7cf31be23c573175072838b06.
    
    Revert "OMS-N: Ensure unique type IDs when parsing overlays [12/14]"
    
    This reverts commit 47e470589f6df73d2eedb0fbcd699e2b65405229.
    
    Revert "OMS-N: Persistence on boot through OverlayManagerServiceImpl [11/14]"
    
    This reverts commit cbf9eb217a3d9cc744cb9e7e294e7230f42c5031.
    
    Revert "OMS-N: Fix memory leak during idmap creation [10/14]"
    
    This reverts commit 3e5e4fc1a7d8311d9a7cf83e13b5ce92398d274a.
    
    Revert "OMS-N: Check invariants after ResTable is modified [9/14]"
    
    This reverts commit ec4487623f578fc9365c36639d69430084288911.
    
    Revert "OMS-N: idmap: suppress print for padded resources [8/14]"
    
    This reverts commit 16dfd6560c742d65a4effb19a6e5064786a594f1.
    
    Revert "OMS-N: pm: set EXTRA_REPLACING correctly in ACTION_PACKAGE_ADDED [7/14]"
    
    This reverts commit c59a5fb4386c8d0131e53beef2ae42e47f1569c0.
    
    Revert "OMS-N: integrate OverlayManagerService into framework [6/14]"
    
    This reverts commit cebc19aba5c18747ed0230e6bd843ae4881762f2.
    
    Revert "OMS-N: introduce the OverlayManagerService [5/14]"
    
    This reverts commit 132a4139532509030f13ab1952cabef60df22115.
    
    Revert "OMS-N: Add ResTable::remove(cookie) [4/14]"
    
    This reverts commit 59fab7acfe7e4875b6cd4a2b77b7879cb04ddecf.
    
    Revert "OMS-N: Make asset manager cookies opaque [3/14]"
    
    This reverts commit 15864a292c260a466d5b4cdde4fd27a9ed8d659e.
    
    Revert "OMS-N: installd: add command 'rmidmap' [2/14]"
    
    This reverts commit 0f3b18c9b081816791b0b2a52e88e6c4a87c306d.
    
    Revert "OMS-N: Support downloaded overlay packages [1/14]"
    
    This reverts commit 9c8856e29fcbabfb7c029af79cca7f05c1c205db.
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -24,7 +24,6 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.R;
-import com.android.internal.util.ThemeUtils;
 import com.android.internal.widget.LockPatternUtils;
 
 import android.app.ActivityManager;
@@ -113,7 +112,6 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private final Context mContext;
     private final WindowManagerFuncs mWindowManagerFuncs;
-    private Context mUiContext;
     private final AudioManager mAudioManager;
     private final IDreamManager mDreamManager;
 
@@ -270,12 +268,6 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 Settings.System.POWER_MENU_ANIMATIONS, 0);
     }
 
-    private Context getUiContext() {
-        mUiContext = ThemeUtils.createUiContext(mContext);
-        mUiContext.setTheme(android.R.style.Theme_DeviceDefault_Light_DarkActionBar);
-        return mUiContext != null ? mUiContext : mContext;
-    }
-
     /**
      * Create the global actions dialog.
      * @return A new dialog.
@@ -416,12 +408,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mAdapter = new MyAdapter();
 
-        AlertParams params = new AlertParams(getUiContext());
+        AlertParams params = new AlertParams(mContext);
         params.mAdapter = mAdapter;
         params.mOnClickListener = this;
         params.mForceInverseBackground = true;
 
-        GlobalActionsDialog dialog = new GlobalActionsDialog(getUiContext(), params);
+        GlobalActionsDialog dialog = new GlobalActionsDialog(mContext, params);
         dialog.setCanceledOnTouchOutside(false); // Handled by the custom class.
 
         dialog.getListView().setItemsCanFocus(true);
@@ -1005,8 +997,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         public View getView(int position, View convertView, ViewGroup parent) {
             Action action = getItem(position);
-            final Context context = getUiContext();
-            return action.create(context, convertView, parent, LayoutInflater.from(context));
+            return action.create(mContext, convertView, parent, LayoutInflater.from(mContext));
         }
     }
 
--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -53,7 +53,6 @@ import android.system.Os;
 import android.provider.Settings;
 import android.widget.ListView;
 
-import com.android.internal.util.ThemeUtils;
 import com.android.internal.telephony.ITelephony;
 import com.android.server.pm.PackageManagerService;
 
@@ -146,7 +145,7 @@ public final class ShutdownThread extends Thread {
         mReboot = false;
         mRebootSafeMode = false;
         mReason = reason;
-        shutdownInner(getUiContext(context), confirm);
+        shutdownInner(context, confirm);
     }
 
     private static boolean isAdvancedRebootPossible(final Context context) {
@@ -201,13 +200,12 @@ public final class ShutdownThread extends Thread {
         if (confirm) {
             final CloseDialogReceiver closer = new CloseDialogReceiver(context);
             final boolean advancedReboot = isAdvancedRebootPossible(context);
-            final Context mUiContext = getUiContext(context);
 
             if (sConfirmDialog != null) {
                 sConfirmDialog.dismiss();
                 sConfirmDialog = null;
             }
-            AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(mUiContext)
+            AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(context)
                     .setTitle(mRebootSafeMode
                             ? com.android.internal.R.string.reboot_safemode_title
                             : showRebootOption
@@ -368,7 +366,7 @@ public final class ShutdownThread extends Thread {
         mRebootSafeMode = false;
         mRebootHasProgressBar = false;
         mReason = reason;
-        shutdownInner(getUiContext(context), confirm);
+        shutdownInner(context, confirm);
     }
 
     /**
@@ -921,12 +919,5 @@ public final class ShutdownThread extends Thread {
         if (!done[0]) {
             Log.w(TAG, "Timed out waiting for uncrypt.");
         }
-    }
-
-    private static Context getUiContext(Context context) {
-        Context mUiContext = null;
-        mUiContext = ThemeUtils.createUiContext(context);
-        mUiContext.setTheme(android.R.style.Theme_DeviceDefault_Light_DarkActionBar);
-        return mUiContext != null ? mUiContext : context;
     }
 }
