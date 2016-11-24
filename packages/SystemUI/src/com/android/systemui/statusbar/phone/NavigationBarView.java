22ea0ab0d35b658e0483dbee3be7ba76334f1170
Bangbang Huang <huangbangbang@huawei.com>
2016-11-24 22:49:27 +0800

    Add new feature of One-hand UI Mode
    Bug: 33118157
    Test: builds, and test feature in bug 33118157
    
    Change-Id: Ic1b62a8d0821385cfe05818c72f9554588457a65
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -56,6 +56,7 @@ import com.android.systemui.RecentsComponent;
 import com.android.systemui.stackdivider.Divider;
 import com.android.systemui.statusbar.policy.DeadZone;
 import com.android.systemui.tuner.TunerService;
+import com.android.systemui.singlehandmode.SlideTouchEvent;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -114,6 +115,7 @@ public class NavigationBarView extends LinearLayout {
 
     private final SparseArray<ButtonDispatcher> mButtonDisatchers = new SparseArray<>();
     private Configuration mConfiguration;
+    private SlideTouchEvent mSlideTouchEvent;
 
     private NavigationBarInflaterView mNavigationInflaterView;
 
@@ -206,7 +208,7 @@ public class NavigationBarView extends LinearLayout {
         mVertical = false;
         mShowMenu = false;
         mGestureHelper = new NavigationBarGestureHelper(context);
-
+        mSlideTouchEvent = new SlideTouchEvent(context);
         mConfiguration = new Configuration();
         mConfiguration.updateFrom(context.getResources().getConfiguration());
         updateIcons(context, Configuration.EMPTY, mConfiguration);
@@ -249,6 +251,7 @@ public class NavigationBarView extends LinearLayout {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        mSlideTouchEvent.handleTouchEvent(event);
         boolean doubleTapNav = Settings.System.getIntForUser(
                       mContext.getContentResolver(), Settings.System.DOUBLE_TAP_SLEEP_NAVBAR, 0, UserHandle.USER_CURRENT) == 1;
         if (mGestureHelper.onTouchEvent(event)) {
@@ -269,6 +272,7 @@ public class NavigationBarView extends LinearLayout {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
+        mSlideTouchEvent.handleTouchEvent(event);
         return mGestureHelper.onInterceptTouchEvent(event);
     }
 
