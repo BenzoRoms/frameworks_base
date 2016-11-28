omnirom/android_frameworks_base_old

base: Allow screen unpinning on devices without navbar
Change-Id: I69dee4e6e0532ab5e0b0e01cbca1dc25e8fb039b

packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
@@ -42,6 +42,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.systemui.R;
+import com.android.internal.util.omni.DeviceUtils;
 
 import java.util.ArrayList;
 
@@ -230,12 +231,27 @@ public class ScreenPinningRequest implements View.OnClickListener {
                         .setVisibility(View.INVISIBLE);
             }
 
+            int description =  R.string.screen_pinning_description;
+            if (!DeviceUtils.deviceSupportNavigationBar(mContext)) {
+                description = R.string.screen_pinning_description_back;
+            } else if (mAccessibilityService.isEnabled()) {
+                description = R.string.screen_pinning_description_accessible;
+            }
+            boolean showBackButton = !DeviceUtils.deviceSupportNavigationBar(mContext) || !mAccessibilityService.isEnabled();
+            boolean showRecentsButton = DeviceUtils.deviceSupportNavigationBar(mContext) && !mAccessibilityService.isEnabled();
+            boolean showSingleButtonMessage = !DeviceUtils.deviceSupportNavigationBar(mContext) || mAccessibilityService.isEnabled();
+
             ((TextView) mLayout.findViewById(R.id.screen_pinning_description))
                     .setText(R.string.screen_pinning_description);
             final int backBgVisibility =
-                    mAccessibilityService.isEnabled() ? View.INVISIBLE : View.VISIBLE;
+                    showBackButton ? View.VISIBLE : View.INVISIBLE;
             mLayout.findViewById(R.id.screen_pinning_back_bg).setVisibility(backBgVisibility);
             mLayout.findViewById(R.id.screen_pinning_back_bg_light).setVisibility(backBgVisibility);
+
+            final int recentsBgVisibility =
+                    showRecentsButton ? View.VISIBLE : View.INVISIBLE;
+            mLayout.findViewById(R.id.screen_pinning_recents_bg).setVisibility(recentsBgVisibility);
+            mLayout.findViewById(R.id.screen_pinning_recents_bg_light).setVisibility(recentsBgVisibility);
 
             addView(mLayout, getRequestLayoutParams(isLandscape));
         }

BenzoRoms/frameworks_base

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

packages/SystemUI/src/com/android/systemui/recents/ScreenPinningRequest.java
@@ -42,6 +42,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.systemui.R;
+import com.android.internal.util.slim.DeviceUtils;
 
 import java.util.ArrayList;
 
@@ -230,12 +231,25 @@ public class ScreenPinningRequest implements View.OnClickListener {
                         .setVisibility(View.INVISIBLE);
             }
 
+            int description =  R.string.screen_pinning_description;
+            if (!DeviceUtils.deviceSupportNavigationBar(mContext)) {
+                description = R.string.screen_pinning_description_back;
+            }
+            boolean showBackButton = !DeviceUtils.deviceSupportNavigationBar(mContext) || !mAccessibilityService.isEnabled();
+            boolean showRecentsButton = DeviceUtils.deviceSupportNavigationBar(mContext) && !mAccessibilityService.isEnabled();
+            boolean showSingleButtonMessage = !DeviceUtils.deviceSupportNavigationBar(mContext) || mAccessibilityService.isEnabled();
+
             ((TextView) mLayout.findViewById(R.id.screen_pinning_description))
                     .setText(R.string.screen_pinning_description);
             final int backBgVisibility =
-                    mAccessibilityService.isEnabled() ? View.INVISIBLE : View.VISIBLE;
+                    showBackButton ? View.VISIBLE : View.INVISIBLE;
             mLayout.findViewById(R.id.screen_pinning_back_bg).setVisibility(backBgVisibility);
             mLayout.findViewById(R.id.screen_pinning_back_bg_light).setVisibility(backBgVisibility);
+
+            final int recentsBgVisibility =
+                    showRecentsButton ? View.VISIBLE : View.INVISIBLE;
+            mLayout.findViewById(R.id.screen_pinning_back_bg).setVisibility(recentsBgVisibility);
+            mLayout.findViewById(R.id.screen_pinning_back_bg_light).setVisibility(recentsBgVisibility);
 
             addView(mLayout, getRequestLayoutParams(isLandscape));
         }