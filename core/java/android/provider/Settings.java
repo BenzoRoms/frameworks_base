b9a5caaa7e1def54fbbcd88191b5b2404a55d1eb
cristianomatos <cristianobmatos@gmail.com>
2016-12-07 14:50:41 +0800

    Recents Clear All Button Tweaks (squashed) [1/2]
    Thanks to RR and @varund7726
    
    Extracted, cleaned and updated for Nitrogen OS by @xyyx
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4836,6 +4836,18 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * show clear all recents button
+         * @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
