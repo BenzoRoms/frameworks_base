Neoteric-OS/android_frameworks_base

Parse Exif OffsetTime in MediaScanner
MediaScanner parses Exif OffsetTime value to use UTC time
for DATE_TAKEN value.

Bug: 62856173
Change-Id: I797d6095600516bea3f1d44bcc64daf00887d03d

media/java/android/media/ExifInterface.java
@@ -215,6 +215,12 @@ public class ExifInterface {
     public static final String TAG_METERING_MODE = "MeteringMode";
     /** Type is String. */
     public static final String TAG_OECF = "OECF";
+    /** Type is String. */
+    public static final String TAG_OFFSET_TIME = "OffsetTime";
+    /** Type is String. */
+    public static final String TAG_OFFSET_TIME_ORIGINAL = "OffsetTimeOriginal";
+    /** Type is String. */
+    public static final String TAG_OFFSET_TIME_DIGITIZED = "OffsetTimeDigitized";
     /** Type is int. */
     public static final String TAG_PIXEL_X_DIMENSION = "PixelXDimension";
     /** Type is int. */
@@ -374,6 +380,7 @@ public class ExifInterface {
     private static final int JPEG_SIGNATURE_SIZE = 3;
 
     private static SimpleDateFormat sFormatter;
+    private static SimpleDateFormat sFormatterTz;
 
     // See Exchangeable image file format for digital still cameras: Exif version 2.2.
     // The following values are for parsing EXIF data area. There are tag groups in EXIF data area.
@@ -861,6 +868,9 @@ public class ExifInterface {
             new ExifTag(TAG_EXIF_VERSION, 36864, IFD_FORMAT_STRING),
             new ExifTag(TAG_DATETIME_ORIGINAL, 36867, IFD_FORMAT_STRING),
             new ExifTag(TAG_DATETIME_DIGITIZED, 36868, IFD_FORMAT_STRING),
+            new ExifTag(TAG_OFFSET_TIME, 36880, IFD_FORMAT_STRING),
+            new ExifTag(TAG_OFFSET_TIME_ORIGINAL, 36881, IFD_FORMAT_STRING),
+            new ExifTag(TAG_OFFSET_TIME_DIGITIZED, 36882, IFD_FORMAT_STRING),
             new ExifTag(TAG_COMPONENTS_CONFIGURATION, 37121, IFD_FORMAT_UNDEFINED),
             new ExifTag(TAG_COMPRESSED_BITS_PER_PIXEL, 37122, IFD_FORMAT_URATIONAL),
             new ExifTag(TAG_SHUTTER_SPEED_VALUE, 37377, IFD_FORMAT_SRATIONAL),
@@ -1059,6 +1069,8 @@ public class ExifInterface {
         nativeInitRaw();
         sFormatter = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
         sFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        sFormatterTz = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss X");
+        sFormatterTz.setTimeZone(TimeZone.getTimeZone("UTC"));
 
         // Build up the hash tables to look up Exif tags for reading Exif tags.
         for (int hint = 0; hint < EXIF_TAGS.length; ++hint) {
@@ -1733,22 +1745,29 @@ public class ExifInterface {
     }
 
     /**
-     * Returns number of milliseconds since Jan. 1, 1970, midnight local time.
-     * Returns -1 if the date time information if not available.
+     * Returns number of milliseconds since Jan. 1, 1970, midnight local time and
+     * number of milliseconds since Jan. 1, 1970, midnight UTC in a long array.
+     * The first element is a local time and the second elemenet is the UTC time.
+     * -1 is set if the date time information is not available.
+     *
      * @hide
      */
-    public long getDateTime() {
+    public long[] getDateTime() {
+        long[] output = new long[2];
+        output[0] = -1;
+        output[1] = -1;
+
         String dateTimeString = getAttribute(TAG_DATETIME);
         if (dateTimeString == null
-                || !sNonZeroTimePattern.matcher(dateTimeString).matches()) return -1;
+                || !sNonZeroTimePattern.matcher(dateTimeString).matches()) return output;
 
         ParsePosition pos = new ParsePosition(0);
         try {
-            // The exif field is in local time. Parsing it as if it is UTC will yield time
-            // since 1/1/1970 local time
-            Date datetime = sFormatter.parse(dateTimeString, pos);
-            if (datetime == null) return -1;
-            long msecs = datetime.getTime();
+            // The exif date time field is in local time. Parsing it as if it is UTC will yield
+            // time since 1/1/1970 local time
+            Date localtime = sFormatter.parse(dateTimeString, pos);
+            if (localtime == null) return output;
+            long localtimeMsecs = localtime.getTime();
 
             String subSecs = getAttribute(TAG_SUBSEC_TIME);
             if (subSecs != null) {
@@ -1757,15 +1776,27 @@ public class ExifInterface {
                     while (sub > 1000) {
                         sub /= 10;
                     }
-                    msecs += sub;
+                    localtimeMsecs += sub;
                 } catch (NumberFormatException e) {
                     // Ignored
                 }
             }
-            return msecs;
+            output[0] = localtimeMsecs;
+
+            String tzString = getAttribute(TAG_OFFSET_TIME);
+            if (tzString != null) {
+                dateTimeString = dateTimeString + " " + tzString;
+                ParsePosition position = new ParsePosition(0);
+                Date utctime = sFormatterTz.parse(dateTimeString, position);
+                if (utctime != null) {
+                    long offsetMsecs = utctime.getTime() - localtime.getTime();
+                    output[1] = localtimeMsecs + offsetMsecs;
+                }
+            }
         } catch (IllegalArgumentException e) {
-            return -1;
+            // Ignored
         }
+        return output;
     }
 
     /**

BenzoRoms/frameworks_base

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

media/java/android/media/ExifInterface.java
@@ -215,6 +215,12 @@ public class ExifInterface {
     public static final String TAG_METERING_MODE = "MeteringMode";
     /** Type is String. */
     public static final String TAG_OECF = "OECF";
+    /** Type is String. */
+    public static final String TAG_OFFSET_TIME = "OffsetTime";
+    /** Type is String. */
+    public static final String TAG_OFFSET_TIME_ORIGINAL = "OffsetTimeOriginal";
+    /** Type is String. */
+    public static final String TAG_OFFSET_TIME_DIGITIZED = "OffsetTimeDigitized";
     /** Type is int. */
     public static final String TAG_PIXEL_X_DIMENSION = "PixelXDimension";
     /** Type is int. */
@@ -374,6 +380,7 @@ public class ExifInterface {
     private static final int JPEG_SIGNATURE_SIZE = 3;
 
     private static SimpleDateFormat sFormatter;
+    private static SimpleDateFormat sFormatterTz;
 
     // See Exchangeable image file format for digital still cameras: Exif version 2.2.
     // The following values are for parsing EXIF data area. There are tag groups in EXIF data area.
@@ -861,6 +868,9 @@ public class ExifInterface {
             new ExifTag(TAG_EXIF_VERSION, 36864, IFD_FORMAT_STRING),
             new ExifTag(TAG_DATETIME_ORIGINAL, 36867, IFD_FORMAT_STRING),
             new ExifTag(TAG_DATETIME_DIGITIZED, 36868, IFD_FORMAT_STRING),
+            new ExifTag(TAG_OFFSET_TIME, 36880, IFD_FORMAT_STRING),
+            new ExifTag(TAG_OFFSET_TIME_ORIGINAL, 36881, IFD_FORMAT_STRING),
+            new ExifTag(TAG_OFFSET_TIME_DIGITIZED, 36882, IFD_FORMAT_STRING),
             new ExifTag(TAG_COMPONENTS_CONFIGURATION, 37121, IFD_FORMAT_UNDEFINED),
             new ExifTag(TAG_COMPRESSED_BITS_PER_PIXEL, 37122, IFD_FORMAT_URATIONAL),
             new ExifTag(TAG_SHUTTER_SPEED_VALUE, 37377, IFD_FORMAT_SRATIONAL),
@@ -1059,6 +1069,8 @@ public class ExifInterface {
         nativeInitRaw();
         sFormatter = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
         sFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        sFormatterTz = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss XXX");
+        sFormatterTz.setTimeZone(TimeZone.getTimeZone("UTC"));
 
         // Build up the hash tables to look up Exif tags for reading Exif tags.
         for (int hint = 0; hint < EXIF_TAGS.length; ++hint) {
@@ -1733,22 +1745,29 @@ public class ExifInterface {
     }
 
     /**
-     * Returns number of milliseconds since Jan. 1, 1970, midnight local time.
-     * Returns -1 if the date time information if not available.
+     * Returns number of milliseconds since Jan. 1, 1970, midnight local time and
+     * number of milliseconds since Jan. 1, 1970, midnight UTC in a long array.
+     * The first element is a local time and the second elemenet is the UTC time.
+     * -1 is set if the date time information is not available.
+     *
      * @hide
      */
-    public long getDateTime() {
+    public long[] getDateTime() {
+        long[] output = new long[2];
+        output[0] = -1;
+        output[1] = -1;
+
         String dateTimeString = getAttribute(TAG_DATETIME);
         if (dateTimeString == null
-                || !sNonZeroTimePattern.matcher(dateTimeString).matches()) return -1;
+                || !sNonZeroTimePattern.matcher(dateTimeString).matches()) return output;
 
         ParsePosition pos = new ParsePosition(0);
         try {
-            // The exif field is in local time. Parsing it as if it is UTC will yield time
-            // since 1/1/1970 local time
-            Date datetime = sFormatter.parse(dateTimeString, pos);
-            if (datetime == null) return -1;
-            long msecs = datetime.getTime();
+            // The exif date time field is in local time. Parsing it as if it is UTC will yield
+            // time since 1/1/1970 local time
+            Date localtime = sFormatter.parse(dateTimeString, pos);
+            if (localtime == null) return output;
+            long localtimeMsecs = localtime.getTime();
 
             String subSecs = getAttribute(TAG_SUBSEC_TIME);
             if (subSecs != null) {
@@ -1757,15 +1776,27 @@ public class ExifInterface {
                     while (sub > 1000) {
                         sub /= 10;
                     }
-                    msecs += sub;
+                    localtimeMsecs += sub;
                 } catch (NumberFormatException e) {
                     // Ignored
                 }
             }
-            return msecs;
+            output[0] = localtimeMsecs;
+
+            String tzString = getAttribute(TAG_OFFSET_TIME);
+            if (tzString != null) {
+                dateTimeString = dateTimeString + " " + tzString;
+                ParsePosition position = new ParsePosition(0);
+                Date utctime = sFormatterTz.parse(dateTimeString, position);
+                if (utctime != null) {
+                    long offsetMsecs = utctime.getTime() - localtime.getTime();
+                    output[1] = localtimeMsecs + offsetMsecs;
+                }
+            }
         } catch (IllegalArgumentException e) {
-            return -1;
+            // Ignored
         }
+        return output;
     }
 
     /**