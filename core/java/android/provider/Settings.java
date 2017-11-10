e5805034d3b16c927857adf17ec9ce6672a265a3
mydongistiny <jaysonedson@gmail.com>
2017-11-10 01:07:19 -0800

    Revert "SystemUI: Forward-port notification counters [1/2]"
    This reverts commit c4344e77edaf3a09744a2cc1326207675a5b53e8.
    
    Change-Id: I2864290ecb0793f2a5df2ae02502377e8fc6dd2b
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4840,12 +4840,6 @@ public final class Settings {
         public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
 
         /**
-         * Show the pending notification counts as overlays on the status bar
-         * @hide
-         */
-        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
-
-        /**
          * navbar recents style - 0 default AOSP (card) 1 OmniSwitch 2 default AOSP (grid)
          * @hide
          */
@@ -5063,7 +5057,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_MEDIUM_COLOR);
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_FULL_COLOR);
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_REALLY_FULL_COLOR);
-            PRIVATE_SETTINGS.add(STATUS_BAR_NOTIF_COUNT);
             PRIVATE_SETTINGS.add(RECENTS_USE_OMNISWITCH);
         }
 
